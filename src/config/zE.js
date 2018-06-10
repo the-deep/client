5505e41ce7badbf47ecad0ecbcb54165c8fd0348
Navin Ayer <ayernavin@togglecorp.com>
2018-06-10 11:39:45 +0545

    Replace JIRA Issue Tracker with Zendesk Widget

---
+++ src/config/zE.js
@@ -0,0 +1,24 @@
+// NOTE: User info and lang is set just for auto fill
+// NOTE: so no need to deactivate on development
+const enableZe = () => window.zE; // process.env.NODE_ENV === 'production'
+
+export const setZeUser = (user) => {
+    if (enableZe() && user.userId) {
+        window.zE(() => {
+            window.zE.identify({
+                id: user.userId,
+                name: user.displayName,
+                email: user.email,
+                isSuperuser: user.isSuperuser,
+            });
+        });
+    }
+};
+
+export const setZeLang = (lang) => {
+    if (enableZe()) {
+        window.zE(() => {
+            window.zE.setLocale(lang);
+        });
+    }
+};
