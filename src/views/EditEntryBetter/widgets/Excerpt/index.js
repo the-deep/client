b4911585de98fcf4a1ff5c055b90302c4f773321
frozenhelium <fren.ankit@gmail.com>
2018-06-20 15:08:35 +0545

    Add new Excerpt widget

--- src/views/EditEntryBetter/widgets/Excerpt/index.js
+++ src/views/EditEntryBetter/widgets/Excerpt/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import TextArea from '#rs/components/Input/TextArea';
+import TextArea from '#rsci/TextArea';
+
+import DropContainer from './DropContainer';
+import styles from './styles.scss';
 
 const propTypes = {
+    className: PropTypes.string,
     entryType: PropTypes.string.isRequired,
     excerpt: PropTypes.string,
     image: PropTypes.string,
@@ -11,50 +15,151 @@ const propTypes = {
 };
 
 const defaultProps = {
+    className: '',
     excerpt: undefined,
     image: undefined,
 };
 
-export default class ExcerptWidget extends React.PureComponent {
+const TEXT = 'excerpt';
+const IMAGE = 'image';
+
+export default class Excerpt extends React.PureComponent {
     static propTypes = propTypes;
     static defaultProps = defaultProps;
 
-    handleExcerptChange = (value) => {
-        const { entryType } = this.props;
-        if (entryType === 'excerpt') {
-            this.props.onExcerptChange({
-                entryType,
-                excerpt: value,
-            });
-        }
+    constructor(props) {
+        super(props);
+        this.state = { isBeingDraggedOver: false };
     }
 
-    render() {
+    handleExcerptChange = (type, value) => {
+        const { onExcerptChange } = this.props;
+        onExcerptChange({
+            type,
+            value,
+        });
+    }
+
+    handleDragEnter = () => {
+        this.setState({ isBeingDraggedOver: true });
+    }
+
+    handleDragOver = (e) => {
+        e.preventDefault();
+    }
+
+    handleDragExit = () => {
+        this.setState({ isBeingDraggedOver: false });
+    }
+
+    handleTextChange = (value) => {
+        this.handleExcerptChange(TEXT, value);
+    }
+
+    handleDragDrop = (e) => {
+        e.preventDefault();
+        const transferredData = e.dataTransfer.getData('text');
+        let formattedData;
+
+        try {
+            formattedData = JSON.parse(transferredData);
+        } catch (ex) {
+            formattedData = {
+                type: TEXT,
+                transferredData,
+            };
+        }
+
+        const {
+            type,
+            data,
+        } = formattedData;
+
+        this.handleExcerptChange(type, data);
+
+        this.setState({ isBeingDraggedOver: false });
+    }
+
+    renderImage = () => {
         const {
             entryType,
-            excerpt,
             image,
         } = this.props;
 
+        const { isBeingDraggedOver } = this.state;
+
+        if (isBeingDraggedOver || entryType !== IMAGE) {
+            return null;
+        }
+
+        const className = `
+            ${styles.image}
+            image 
+        `;
+
         // FIXME: use translation
-        const alt = 'Excerpt image';
+        const altText = 'Excerpt image';
+        return (
+            <img
+                className={className}
+                src={image}
+                alt={altText}
+            />
+        );
+    }
+
+    renderText = () => {
+        const {
+            entryType,
+            excerpt,
+        } = this.props;
+
+        const { isBeingDraggedOver } = this.state;
+
+        if (isBeingDraggedOver || entryType !== TEXT) {
+            return null;
+        }
+
+        const className = `
+            ${styles.text}
+            text
+        `;
 
         return (
-            <div>
-                {
-                    entryType === 'image' ? (
-                        <img
-                            src={image}
-                            alt={alt}
-                        />
-                    ) : (
-                        <TextArea
-                            showLabel={false}
-                            value={excerpt}
-                            onChange={this.handleExcerptChange}
-                        />
-                    )
-                }
+            <TextArea
+                className={className}
+                showLabel={false}
+                showHintAndError={false}
+                value={excerpt}
+                onChange={this.handleTextChange}
+            />
+        );
+    }
+
+    render() {
+        const { className: classNameFromProps } = this.props;
+        const { isBeingDraggedOver } = this.state;
+
+        const Image = this.renderImage;
+        const Text = this.renderText;
+
+        const className = `
+            ${classNameFromProps}
+            ${styles.excerpt}
+            excerpt
+        `;
+
+        return (
+            <div
+                className={className}
+                onDragEnter={this.handleDragEnter}
+                onDragLeave={this.handleDragExit}
+                onDragOver={this.handleDragOver}
+                onDrop={this.handleDragDrop}
+            >
+                <DropContainer show={isBeingDraggedOver} />
+                <Text />
+                <Image />
             </div>
         );
     }
