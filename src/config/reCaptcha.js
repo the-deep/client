a397aabf9b7830c8f329cc088c37d5577c2e47f8
Navin Ayer <ayernavin@togglecorp.com>
2018-07-11 11:06:52 +0545

    Update recaptcha for dev
    * Add recaptcha error to lang

--- src/config/reCaptcha.js
+++ src/config/reCaptcha.js
@@ -1,6 +1,6 @@
 const getSiteKey = () => {
     if (process.env.NODE_ENV === 'development') {
-        return '6LcsukYUAAAAAIUqQIAAx966JSRnwhdWbR4V-LTb';
+        return '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
     }
     return '6LfhyEYUAAAAAObS-Vi5g_E4SFDfGnt_iswkJ9r8';
 };
