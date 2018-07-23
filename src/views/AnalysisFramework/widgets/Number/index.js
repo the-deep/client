3b6116fa512f1715d35bc94c64ad198c1ec30754
tnagorra <weathermist@gmail.com>
2018-07-23 12:29:14 +0545

    Add number input

---
+++ src/views/AnalysisFramework/widgets/Number/index.js
@@ -0,0 +1,135 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import NumberInput from '#rs/components/Input/NumberInput';
+import TextInput from '#rs/components/Input/TextInput';
+import Button from '#rs/components/Action/Button';
+import PrimaryButton from '#rs/components/Action/Button/PrimaryButton';
+import Modal from '#rs/components/View/Modal';
+import ModalHeader from '#rs/components/View/Modal/Header';
+import ModalBody from '#rs/components/View/Modal/Body';
+import ModalFooter from '#rs/components/View/Modal/Footer';
+import BoundError from '#rs/components/General/BoundError';
+
+import WidgetError from '#components/WidgetError';
+import _ts from '#ts';
+
+import styles from './styles.scss';
+
+const propTypes = {
+    title: PropTypes.string.isRequired,
+    editAction: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+};
+
+const defaultProps = {
+};
+
+@BoundError(WidgetError)
+export default class NumberFrameworkList extends React.PureComponent {
+    static propTypes = propTypes;
+    static defaultProps = defaultProps;
+
+    constructor(props) {
+        super(props);
+
+        const {
+            title,
+            editAction,
+        } = this.props;
+
+        this.state = {
+            showEditModal: false,
+            title,
+        };
+
+        editAction(this.handleEdit);
+    }
+
+    handleWidgetTitleChange = (value) => {
+        this.setState({ title: value });
+    }
+
+    handleEdit = () => {
+        this.setState({ showEditModal: true });
+    }
+
+    handleModalCancelButtonClick = () => {
+        this.setState({
+            showEditModal: false,
+            title: this.props.title,
+        });
+    }
+
+    handleModalSaveButtonClick = () => {
+        this.setState({ showEditModal: false });
+        const { title } = this.state;
+        const { onChange } = this.props;
+
+        onChange(
+            undefined,
+            title,
+        );
+    }
+
+    renderEditModal = () => {
+        const {
+            title,
+            showEditModal,
+        } = this.state;
+
+        if (!showEditModal) {
+            return null;
+        }
+
+        const headerTitle = _ts('framework.numberWidget', 'editTitleModalHeader');
+        const titleInputLabel = _ts('framework.numberWidget', 'titleLabel');
+        const titleInputPlaceholder = _ts('framework.numberWidget', 'widgetTitlePlaceholder');
+        const cancelButtonLabel = _ts('framework.numberWidget', 'cancelButtonLabel');
+        const saveButtonLabel = _ts('framework.numberWidget', 'saveButtonLabel');
+
+        return (
+            <Modal>
+                <ModalHeader title={headerTitle} />
+                <ModalBody>
+                    <TextInput
+                        label={titleInputLabel}
+                        placeholder={titleInputPlaceholder}
+                        onChange={this.handleWidgetTitleChange}
+                        value={title}
+                        showHintAndError={false}
+                        autoFocus
+                        selectOnFocus
+                    />
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick={this.handleModalCancelButtonClick}>
+                        { cancelButtonLabel }
+                    </Button>
+                    <PrimaryButton onClick={this.handleModalSaveButtonClick}>
+                        { saveButtonLabel }
+                    </PrimaryButton>
+                </ModalFooter>
+            </Modal>
+        );
+    }
+
+    render() {
+        const EditModal = this.renderEditModal;
+        const separatorText = ' ';
+
+        return (
+            <div className={styles.list}>
+                <NumberInput
+                    className={styles.input}
+                    placeholder={_ts('framework.numberWidget', 'numberPlaceholder')}
+                    showLabel={false}
+                    showHintAndError={false}
+                    separator={separatorText}
+                    disabled
+                />
+                <EditModal />
+            </div>
+        );
+    }
+}
