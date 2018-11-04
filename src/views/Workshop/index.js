d7dca417c61cc4d6bcaff1a6e4d612bad058820e
Aditya Khatri <aditya.khatri@togglecorp.com>
2018-11-04 14:46:27 +0545

    Add search filed in schema of workshop faram

--- src/views/Workshop/index.js
+++ src/views/Workshop/index.js
@@ -49,6 +49,7 @@ export default class Workshop extends React.PureComponent {
 
     static schema = {
         fields: {
+            search: [],
             username: [
                 requiredCondition,
             ],
@@ -94,6 +95,7 @@ export default class Workshop extends React.PureComponent {
         this.state = {
             faramErrors: {},
             faramValues: {
+                search: 'Looking',
                 username: 'thenav56',
                 password: 'noobnoob',
                 rainbowAffinity: false,
@@ -157,7 +159,6 @@ export default class Workshop extends React.PureComponent {
             // readOnly,
             pending,
         } = this.state;
-
 
         const disabled = faramState === 'disabled';
         const readOnly = faramState === 'readOnly';
@@ -366,7 +367,6 @@ export default class Workshop extends React.PureComponent {
                                 comparator: (a, b) => compareNumber(a.position, b.position),
                             },
                         ]}
-                        hideRemoveFromListButton
                     />
 
                     <PrimaryButton
