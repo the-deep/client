42f370ac92ab57d0e41f43597f51ae10e4854bb0
tnagorra <weathermist@gmail.com>
2019-01-15 11:30:13 +0545

    Removed travis, sentry config related to ravl

--- scripts/vendor-clone.sh
+++ scripts/vendor-clone.sh
@@ -7,11 +7,3 @@ else
     echo "Cloning react-store to src/vendor/react-store"
     git clone git@github.com:toggle-corp/react-store.git src/vendor/react-store
 fi
-
-# Clone react-store if necessary
-if [ -d "src/vendor/ravl" ]; then
-    echo "Skipping ravl (already cloned)"
-else
-    echo "Cloning ravl to src/vendor/ravl"
-    git clone git@github.com:toggle-corp/ravl.git src/vendor/ravl
-fi
