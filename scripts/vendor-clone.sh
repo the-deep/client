0ae077301a6eac71b75cb76bf76d6dd36c7f4850
Navin Ayer <ayernavin@togglecorp.com>
2019-02-10 15:22:58 +0545

    User github for react-components

--- scripts/vendor-clone.sh
+++ scripts/vendor-clone.sh
@@ -7,11 +7,3 @@ else
     echo "Cloning react-store to src/vendor/react-store"
     git clone git@github.com:toggle-corp/react-store.git src/vendor/react-store
 fi
-
-# Clone react-components if necessary
-if [ -d "src/vendor/react-components" ]; then
-    echo "Skipping react-components (already cloned)"
-else
-    echo "Cloning react-components to src/vendor/react-components"
-    git clone git@github.com:timilsinabishal/react-components.git src/vendor/react-components
-fi
