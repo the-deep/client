897a5e739736f1bf28491fb368eaabe156f4ecfd
tnagorra <weathermist@gmail.com>
2019-02-24 18:44:35 +0545

    Remove explicit node-sass installation
    - Fix style for Visualization

--- scripts/run_develop.sh
+++ scripts/run_develop.sh
@@ -2,7 +2,9 @@
 
 DEV_LANG_FILE=/code/src/redux/initial-state/dev-lang.json
 STRING_SERVER_FILE=/code/src/utils/string_server.js
+
+yarn install
+
 node $STRING_SERVER_FILE 5000 $DEV_LANG_FILE &
 
-yarn add --force node-sass@4.7.2
 yarn start
