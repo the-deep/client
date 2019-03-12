b70bf207a5fe2b0e53d73f890cd6d876ca06ae73
Aditya Khatri <aditya.khatri@togglecorp.com>
2019-03-12 10:57:44 +0545

    Change color for geo in tabular viz

--- src/views/LeadAdd/LeadFormItem/LeadTabular/CsvSettings.js
+++ src/views/LeadAdd/LeadFormItem/LeadTabular/CsvSettings.js
@@ -4,8 +4,8 @@ import TextInput from '#rsci/TextInput';
 import Checkbox from '#rsci/Checkbox';
 import _ts from '#ts';
 
-export default () => (
-    <React.Fragment>
+export default ({ className }) => (
+    <div className={className}>
         <TextInput
             faramElementName="delimiter"
             label={_ts('addLeads.tabular', 'delimiterLabel')}
@@ -14,5 +14,5 @@ export default () => (
             faramElementName="noHeaders"
             label={_ts('addLeads.tabular', 'noHeaderLabel')}
         />
-    </React.Fragment>
+    </div>
 );
