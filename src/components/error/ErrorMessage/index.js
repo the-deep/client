39db702cc2353ff2a45e2ca958a8b526c7896882
Aditya Khatri <aditya.khatri@togglecorp.com>
2019-05-03 10:59:32 +0545

    Fix error message component

--- src/components/error/ErrorMessage/index.js
+++ src/components/error/ErrorMessage/index.js
@@ -7,15 +7,23 @@ import Message from '#rscv/Message';
 import { handleException, handleReport } from '#config/sentry';
 
 import Cloak from '#components/general/Cloak';
+import _cs from '#cs';
+
 import styles from './styles.scss';
 
 const propTypes = {
     errorText: PropTypes.string.isRequired,
     reportButtonText: PropTypes.string.isRequired,
+    className: PropTypes.string,
+};
+
+const defaultProps = {
+    className: '',
 };
 
 export default class ErrorMessage extends React.PureComponent {
     static propTypes = propTypes;
+    static defaultProps = defaultProps;
 
     static handleException = handleException;
     static shouldHideReport = ({ isDevMode }) => isDevMode;
@@ -24,23 +32,26 @@ export default class ErrorMessage extends React.PureComponent {
         const {
             errorText,
             reportButtonText,
+            className,
         } = this.props;
 
         return (
-            <Message className={styles.messageContainer}>
-                { errorText }
-                <Cloak
-                    hide={ErrorMessage.shouldHideReport}
-                    render={
-                        <PrimaryButton
-                            onClick={handleReport}
-                            className={styles.button}
-                        >
-                            {reportButtonText}
-                        </PrimaryButton>
-                    }
-                />
-            </Message>
+            <div className={_cs(styles.messageContainer, className)}>
+                <Message>
+                    { errorText }
+                    <Cloak
+                        hide={ErrorMessage.shouldHideReport}
+                        render={
+                            <PrimaryButton
+                                onClick={handleReport}
+                                className={styles.button}
+                            >
+                                {reportButtonText}
+                            </PrimaryButton>
+                        }
+                    />
+                </Message>
+            </div>
         );
     }
 }
