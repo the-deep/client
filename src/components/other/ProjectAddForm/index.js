2ea4c9e959cd92d928cdec2d796b2f16d4056d82
Aditya Khatri <aditya.khatri@togglecorp.com>
2020-12-16 19:41:57 +0545

    Add notify error on project creation form (#1252)
    * Add notify error on project creation form
    
    * Fix review requests in project add form

--- src/components/other/ProjectAddForm/index.js
+++ src/components/other/ProjectAddForm/index.js
@@ -3,9 +3,10 @@
  */
 
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { connect } from 'react-redux';
 import Faram, { requiredCondition } from '@togglecorp/faram';
+import { _cs } from '@togglecorp/fujs';
 
 import NonFieldErrors from '#rsci/NonFieldErrors';
 import TextInput from '#rsci/TextInput';
@@ -15,14 +16,15 @@ import DangerButton from '#rsca/Button/DangerButton';
 import PrimaryButton from '#rsca/Button/PrimaryButton';
 
 import Cloak from '#components/general/Cloak';
+import useRequest from '#utils/request';
 import {
     setProjectAction,
     setUserProjectAction,
     setUsergroupViewProjectAction,
 } from '#redux';
 import _ts from '#ts';
-
-import ProjectCreateRequest from './requests/ProjectCreateRequest';
+import notify from '#notify';
+import { notifyOnFailure } from '#utils/requestNotify';
 
 import styles from './styles.scss';
 
@@ -58,142 +60,130 @@ const mapDispatchToProps = dispatch => ({
     setUsergroupProject: params => dispatch(setUsergroupViewProjectAction(params)),
 });
 
-@connect(undefined, mapDispatchToProps)
-export default class ProjectAddForm extends React.PureComponent {
-    static propTypes = propTypes;
-    static defaultProps = defaultProps;
+const projectAddSchema = {
+    fields: {
+        title: [requiredCondition],
+        isPrivate: [],
+        organizations: [],
+    },
+};
 
-    static shouldHidePrivate = ({ accessPrivateProject }) => !accessPrivateProject;
+function ProjectAddForm(props) {
+    const {
+        className,
+        onModalClose,
+        userGroups,
+        userId,
+        onProjectAdd,
+        setUserProject,
+        setUsergroupProject,
+        setUserProfileProject,
+    } = props;
 
-    constructor(props) {
-        super(props);
+    const [faramValues, setFaramValues] = useState({ isPrivate: false, organizations: [] });
+    const [submitValues, setSubmitValues] = useState({});
+    const [faramErrors, setFaramErrors] = useState({});
+    const [pristine, setPristine] = useState(true);
 
-        this.state = {
-            faramErrors: {},
-            faramValues: {
-                isPrivate: false,
-                organizations: [],
-            },
-            pending: false,
-            pristine: false,
-        };
+    const [
+        pending,
+        ,
+        ,
+        createNewProject,
+    ] = useRequest({
+        url: 'server://projects/',
+        body: submitValues,
+        method: 'POST',
+        onSuccess: (response) => {
+            // FIXME: Remove too many redux writes in future
+            setUserProject({ project: response });
+            setUsergroupProject({ project: response });
+            setUserProfileProject({
+                userId,
+                project: response,
+            });
+            if (onProjectAdd) {
+                onProjectAdd(response.id);
+            }
+            notify.send({
+                title: _ts('components.addProject', 'userProjectCreate'),
+                type: notify.type.SUCCESS,
+                message: _ts('components.addProject', 'userProjectCreateSuccess'),
+                duration: notify.duration.MEDIUM,
+            });
+            onModalClose();
+        },
+        onFailure: (error, errorBody) => {
+            setFaramErrors(errorBody?.faramErrors);
+            notifyOnFailure(_ts('components.addProject', 'addProjectModalLabel'))({ error: errorBody });
+        },
+    });
 
-        this.schema = {
-            fields: {
-                title: [requiredCondition],
-                isPrivate: [],
-                organizations: [],
-            },
-        };
+    const handleFaramChange = useCallback((newFaramValues, newFaramErrors) => {
+        setFaramValues(newFaramValues);
+        setFaramErrors(newFaramErrors);
+        setPristine(false);
+    }, [setFaramValues, setFaramErrors, setPristine]);
 
-        this.projectCreateRequest = new ProjectCreateRequest({
-            setState: v => this.setState(v),
-            setUserProject: this.props.setUserProject,
-            setUserProfileProject: this.props.setUserProfileProject,
-            setUsergroupProject: this.props.setUsergroupProject,
+    const handleFaramFailure = useCallback((newFaramErrors) => {
+        setFaramErrors(newFaramErrors);
+    }, [setFaramErrors]);
 
-            onProjectAdd: this.props.onProjectAdd,
-            onModalClose: this.props.onModalClose,
+    const handleFaramSuccess = useCallback((values) => {
+        setSubmitValues({
+            ...values,
+            userGroups,
         });
-    }
+        createNewProject();
+    }, [createNewProject, userGroups]);
 
-    componentWillUnmount() {
-        this.projectCreateRequest.stop();
-    }
-
-    onModalClose = () => {
-        this.props.onModalClose();
-    }
-
-    // FORM RELATED
-
-    changeCallback = (faramValues, faramErrors) => {
-        this.setState({
-            faramValues,
-            faramErrors,
-            pristine: true,
-        });
-    };
-
-    failureCallback = (faramErrors) => {
-        this.setState({ faramErrors });
-    };
-
-    successCallback = ({
-        title,
-        isPrivate,
-        organizations,
-    }) => {
-        const { userGroups, userId } = this.props;
-        this.projectCreateRequest.init(
-            userId,
-            {
-                title,
-                userGroups,
-                isPrivate,
-                organizations,
-            },
-        ).start();
-    };
-
-    render() {
-        const { className: classNameFromProps } = this.props;
-
-        const {
-            faramValues,
-            faramErrors,
-            pending,
-            pristine,
-        } = this.state;
-
-        const className = `
-            ${classNameFromProps}
-            ${styles.userProjectAddForm}
-        `;
-
-        return (
-            <Faram
-                className={className}
-                onChange={this.changeCallback}
-                onValidationFailure={this.failureCallback}
-                onValidationSuccess={this.successCallback}
-                schema={this.schema}
-                value={faramValues}
-                error={faramErrors}
-                disabled={pending}
-            >
-                { pending && <LoadingAnimation /> }
-                <NonFieldErrors faramElement />
-                <TextInput
-                    faramElementName="title"
-                    label={_ts('components.addProject', 'addProjectModalLabel')}
-                    placeholder={_ts('components.addProject', 'addProjectModalPlaceholder')}
-                    autoFocus
-                />
-                <Cloak
-                    hide={ProjectAddForm.shouldHidePrivate}
-                    render={
-                        <SegmentInput
-                            options={projectVisibilityOptions}
-                            className={styles.isPrivateCheckbox}
-                            faramElementName="isPrivate"
-                            label={_ts('components.addProject', 'projectVisibilityInputLabel')}
-                            hint={_ts('components.addProject', 'projectVisibilityInputHint')}
-                        />
-                    }
-                />
-                <div className={styles.actionButtons}>
-                    <DangerButton onClick={this.onModalClose}>
-                        {_ts('components.addProject', 'modalCancel')}
-                    </DangerButton>
-                    <PrimaryButton
-                        type="submit"
-                        disabled={pending || !pristine}
-                    >
-                        {_ts('components.addProject', 'modalCreate')}
-                    </PrimaryButton>
-                </div>
-            </Faram>
-        );
-    }
+    return (
+        <Faram
+            className={_cs(className, styles.userProjectAddForm)}
+            onChange={handleFaramChange}
+            onValidationFailure={handleFaramFailure}
+            onValidationSuccess={handleFaramSuccess}
+            schema={projectAddSchema}
+            value={faramValues}
+            error={faramErrors}
+            disabled={pending}
+        >
+            { pending && <LoadingAnimation /> }
+            <NonFieldErrors faramElement />
+            <TextInput
+                faramElementName="title"
+                label={_ts('components.addProject', 'addProjectModalLabel')}
+                placeholder={_ts('components.addProject', 'addProjectModalPlaceholder')}
+                autoFocus
+            />
+            <Cloak
+                hide={ProjectAddForm.shouldHidePrivate}
+                render={
+                    <SegmentInput
+                        options={projectVisibilityOptions}
+                        className={styles.isPrivateCheckbox}
+                        faramElementName="isPrivate"
+                        label={_ts('components.addProject', 'projectVisibilityInputLabel')}
+                        hint={_ts('components.addProject', 'projectVisibilityInputHint')}
+                    />
+                }
+            />
+            <div className={styles.actionButtons}>
+                <DangerButton onClick={onModalClose}>
+                    {_ts('components.addProject', 'modalCancel')}
+                </DangerButton>
+                <PrimaryButton
+                    type="submit"
+                    disabled={pending || pristine}
+                >
+                    {_ts('components.addProject', 'modalCreate')}
+                </PrimaryButton>
+            </div>
+        </Faram>
+    );
 }
+
+ProjectAddForm.propTypes = propTypes;
+ProjectAddForm.defaultProps = defaultProps;
+
+export default connect(undefined, mapDispatchToProps)(ProjectAddForm);
