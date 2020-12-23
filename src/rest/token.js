82a733841a16e15c440c2f6e0e2e6219545bee59
Aditya Khatri <aditya.khatri@togglecorp.com>
2020-12-23 12:32:18 +0545

    Refactor login page (#1264)
    * Set maximum supported password to 128 characters

--- src/rest/token.js
+++ src/rest/token.js
@@ -2,19 +2,7 @@ import {
     wsEndpoint,
     POST,
     commonHeaderForPost,
-    commonHeaderForPostExternal,
 } from '#config/rest';
-
-export const urlForTokenCreate = `${wsEndpoint}/token/`;
-export const createParamsForTokenCreate = ({ username, password, recaptchaResponse }) => ({
-    method: POST,
-    headers: commonHeaderForPost,
-    body: JSON.stringify({
-        username,
-        password,
-        recaptchaResponse,
-    }),
-});
 
 export const urlForTokenRefresh = `${wsEndpoint}/token/refresh/`;
 export const createParamsForTokenRefresh = ({ refresh }) => ({
@@ -22,22 +10,5 @@ export const createParamsForTokenRefresh = ({ refresh }) => ({
     headers: commonHeaderForPost,
     body: JSON.stringify({
         refresh,
-    }),
-});
-
-export const urlForTokenCreateHid = `${wsEndpoint}/token/hid/`;
-export const createParamsForTokenCreateHid = ({
-    accessToken,
-    expiresIn,
-    state,
-    tokenType,
-}) => ({
-    method: POST,
-    headers: commonHeaderForPostExternal,
-    body: JSON.stringify({
-        accessToken,
-        expiresIn,
-        state,
-        tokenType,
     }),
 });
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,9 +1,5 @@
-/**
- * @author frozenhelium <fren.ankit@gmail.com>
- */
-
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { reverseRoute } from '@togglecorp/fujs';
@@ -11,46 +7,28 @@ import Faram, {
     requiredCondition,
     emailCondition,
     lengthGreaterThanCondition,
+    lengthLessThanCondition,
 } from '@togglecorp/faram';
 import { parseUrlParams } from '@togglecorp/react-rest-request';
-
-import { FgRestBuilder } from '#rsu/rest';
 
 import Icon from '#rscg/Icon';
 import PrimaryButton from '#rsca/Button/PrimaryButton';
 import NonFieldErrors from '#rsci/NonFieldErrors';
 import ReCaptcha from '#rsci/ReCaptcha';
 import TextInput from '#rsci/TextInput';
+import useRequest from '#utils/request';
 
 import { hidUrl } from '#config/hid';
 import { reCaptchaSiteKey } from '#config/reCaptcha';
 import { pathNames } from '#constants';
 import {
-    alterResponseErrorToFaramError,
-    createParamsForTokenCreate,
-    urlForTokenCreate,
-    createParamsForTokenCreateHid,
-    urlForTokenCreateHid,
-} from '#rest';
-import {
     loginAction,
     authenticateAction,
 } from '#redux';
 import { startSiloBackgroundTasksAction } from '#redux/middlewares/siloBackgroundTasks';
-import schema from '#schema';
 import _ts from '#ts';
 
 import styles from './styles.scss';
-
-const propTypes = {
-    authenticate: PropTypes.func.isRequired,
-    location: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-    login: PropTypes.func.isRequired,
-    startSiloTasks: PropTypes.func.isRequired,
-};
-
-const defaultProps = {
-};
 
 const mapDispatchToProps = dispatch => ({
     authenticate: () => dispatch(authenticateAction()),
@@ -58,282 +36,249 @@ const mapDispatchToProps = dispatch => ({
     startSiloTasks: params => dispatch(startSiloBackgroundTasksAction(params)),
 });
 
-@connect(undefined, mapDispatchToProps)
-export default class Login extends React.PureComponent {
-    static propTypes = propTypes;
-    static defaultProps = defaultProps;
+const schema = {
+    fields: {
+        email: [
+            requiredCondition,
+            emailCondition,
+        ],
+        password: [
+            requiredCondition,
+            lengthGreaterThanCondition(4),
+            lengthLessThanCondition(129),
+        ],
+    },
+};
 
-    static schema = {
-        fields: {
-            email: [
-                requiredCondition,
-                emailCondition,
-            ],
-            password: [
-                requiredCondition,
-                lengthGreaterThanCondition(4),
-            ],
+const schemaWithRecaptcha = {
+    fields: {
+        email: [
+            requiredCondition,
+            emailCondition,
+        ],
+        password: [
+            requiredCondition,
+            lengthGreaterThanCondition(4),
+            lengthLessThanCondition(129),
+        ],
+        recaptchaResponse: [requiredCondition],
+    },
+};
+
+function Login(props) {
+    const {
+        // FIXME: get location value from redux
+        login,
+        authenticate,
+        startSiloTasks,
+        location,
+    } = props;
+
+    const [faramValues, setFaramValues] = useState({});
+    const [faramErrors, setFaramErrors] = useState({});
+    const [pending, setPending] = useState(false);
+    const [loginUrl, setLoginUrl] = useState(false);
+    const [loginBody, setLoginBody] = useState({});
+    const [showReCaptcha, setShowReCaptcha] = useState(false);
+    const [finalSchema, setFinalSchema] = useState(schema);
+    const recaptchaRef = useRef(null);
+
+    const [loginPending, , , loginTrigger] = useRequest({
+        url: loginUrl,
+        method: 'POST',
+        body: loginBody,
+        onSuccess: (response) => {
+            const { refresh, access } = response;
+            login({ refresh, access });
+            startSiloTasks(() => console.log('Silo tasks started'));
+            authenticate();
+            if (recaptchaRef.current && recaptchaRef.current.reset) {
+                recaptchaRef.current.reset();
+            }
         },
-    };
-
-    static schemaWithRecaptcha = {
-        fields: {
-            email: [
-                requiredCondition,
-                emailCondition,
-            ],
-            password: [
-                requiredCondition,
-                lengthGreaterThanCondition(4),
-            ],
-            recaptchaResponse: [requiredCondition],
+        onFailure: (_, { errorCode, faramErrors: newFaramErrors }) => {
+            if (errorCode === 4004) {
+                console.warn('i am here');
+                setFaramErrors({
+                    ...newFaramErrors,
+                    $internal: [
+                        showReCaptcha ? _ts('login', 'retryRecaptcha') : _ts('login', 'enterRecaptcha'),
+                    ],
+                });
+                setPending(false);
+                setShowReCaptcha(true);
+                setFinalSchema(schemaWithRecaptcha);
+            } else {
+                setFaramErrors(newFaramErrors);
+                setPending(false);
+            }
+            if (recaptchaRef.current && recaptchaRef.current.reset) {
+                recaptchaRef.current.reset();
+            }
         },
-    };
+        schemaName: 'tokenGetResponse',
+    });
 
-    constructor(props) {
-        super(props);
+    useEffect(() => {
+        setPending(loginPending);
+    }, [loginPending]);
 
-        this.state = {
-            faramErrors: {},
-            faramValues: {},
-            pending: false,
-            showReCaptcha: false,
-            schema: Login.schema,
-        };
-    }
-
-    componentWillMount() {
-        this.checkParamsFromHid();
-    }
-
-    componentWillUnmount() {
-        if (this.userLoginRequest) {
-            this.userLoginRequest.stop();
-        }
-    }
-
-    // HID
-
-    onHidLoginClick = () => {
+    const onHidLoginClick = useCallback(() => {
         // Just set it to pending
         // The anchor will redirect user to next page
-        this.setState({ pending: true });
-    }
+        setPending(true);
+    }, []);
 
-    setResetRecaptchaFunction = (func) => {
-        this.resetRecaptcha = func;
-    }
-
-    checkParamsFromHid = () => {
-        // FIXME: get location value from redux
-        const { location } = this.props;
+    const checkParamsFromHid = useCallback(() => {
         // Get params from the current url
         // NOTE: hid provides query as hash
         const query = parseUrlParams(location.hash.replace('#', ''));
         // Login User with HID access_token
         if (query.access_token) {
-            const params = createParamsForTokenCreateHid({
+            const params = {
                 accessToken: query.access_token,
                 expiresIn: query.expires_in,
                 state: query.state,
                 tokenType: query.token_type,
-            });
-            this.login({ url: urlForTokenCreateHid, params });
+            };
+            setLoginUrl('server://token/hid/');
+            setLoginBody(params);
+            loginTrigger();
         }
-    }
+    }, [loginTrigger, location]);
 
-    handleFaramChange = (faramValues, faramErrors) => {
-        this.setState({
-            faramValues,
-            faramErrors,
-        });
-    };
+    useEffect(() => {
+        checkParamsFromHid();
+    }, [checkParamsFromHid]);
 
-    handleFaramValidationFailure = (faramErrors) => {
-        this.setState({ faramErrors });
-    };
+    const handleFaramChange = useCallback((newFaramValues, newFaramErrors) => {
+        setFaramValues(newFaramValues);
+        setFaramErrors(newFaramErrors);
+    }, []);
 
-    handleFaramValidationSuccess = (_, { email, password, recaptchaResponse }) => {
-        const url = urlForTokenCreate;
-        const params = createParamsForTokenCreate({
+    const handleFaramValidationFailure = useCallback((newFaramErrors) => {
+        setFaramErrors(newFaramErrors);
+    }, []);
+
+    const handleFaramValidationSuccess = useCallback((
+        _,
+        {
+            email,
+            password,
+            recaptchaResponse,
+        },
+    ) => {
+        const params = {
             username: email,
             password,
             recaptchaResponse,
-        });
-        this.login({ url, params });
-    };
+        };
+        setLoginUrl('server://token/');
+        setLoginBody(params);
+        loginTrigger();
+    }, [loginTrigger]);
 
-    // LOGIN ACTION
-
-    login = ({ url, params }) => {
-        // Stop any retry action
-        if (this.userLoginRequest) {
-            this.userLoginRequest.stop();
-        }
-
-        this.userLoginRequest = this.createRequestLogin(url, params);
-        this.userLoginRequest.start();
-    };
-
-    // LOGIN REST API
-
-    createRequestLogin = (url, params) => {
-        const userLoginRequest = new FgRestBuilder()
-            .url(url)
-            .params(params)
-            .preLoad(() => {
-                this.setState({ pending: true });
-            })
-            .postLoad(() => {
-                if (this.resetRecaptcha) {
-                    this.resetRecaptcha();
-                }
-            })
-            .success((response) => {
-                try {
-                    schema.validate(response, 'tokenGetResponse');
-
-                    const { refresh, access } = response;
-                    this.props.login({ refresh, access });
-                    this.props.startSiloTasks(() => console.log('Silo tasks started'));
-                    this.props.authenticate();
-                } catch (err) {
-                    console.error(err);
-                }
-            })
-            .failure((response) => {
-                const faramErrors = alterResponseErrorToFaramError(response.errors);
-                if (response.errorCode === 4004) {
-                    const { showReCaptcha } = this.state;
-                    this.setState({
-                        faramErrors: {
-                            ...faramErrors,
-                            $internal: [
-                                showReCaptcha ? _ts('login', 'retryRecaptcha') : _ts('login', 'enterRecaptcha'),
-                            ],
-                        },
-                        pending: false,
-                        showReCaptcha: true,
-                        schema: Login.schemaWithRecaptcha,
-                    });
-                } else {
-                    this.setState({
-                        faramErrors,
-                        pending: false,
-                    });
-                }
-            })
-            .fatal(() => {
-                this.setState({
-                    faramErrors: { $internal: [_ts('login', 'loginError')] },
-                    pending: false,
-                });
-            })
-            .build();
-        return userLoginRequest;
-    }
-
-    render() {
-        const {
-            faramErrors,
-            faramValues,
-            pending,
-            showReCaptcha,
-        } = this.state;
-
-        return (
-            <div className={styles.login}>
-                <div className={styles.deepContainer}>
-                    <Icon
-                        className={styles.logo}
-                        name="deepLogo"
-                    />
-                    <h2 className={styles.heading}>
-                        <small>{_ts('login', 'welcomeToText')}</small><br />
-                    </h2>
-                </div>
-                <div className={styles.loginFormContainer}>
-                    <div className={styles.hidLinkContainer}>
-                        <a
-                            className={styles.hidLink}
-                            href={hidUrl}
-                            onClick={this.onHidLoginClick}
-                        >
-                            <Icon
-                                className={styles.logo}
-                                name="hidLogo"
-                                alt={_ts('login', 'logInWIthHid')}
-                                draggable="false"
-                            />
-                            <span>
-                                {_ts('login', 'logInWIthHid')}
-                            </span>
-                        </a>
-                        <div className={styles.orContainer}>
-                            <hr />
-                            <span className={styles.or}>
-                                {_ts('login', 'orText')}
-                            </span>
-                        </div>
-                    </div>
-                    <Faram
-                        className={styles.loginForm}
-                        onChange={this.handleFaramChange}
-                        onValidationFailure={this.handleFaramValidationFailure}
-                        onValidationSuccess={this.handleFaramValidationSuccess}
-                        schema={this.state.schema}
-                        value={faramValues}
-                        error={faramErrors}
-                        disabled={pending}
+    return (
+        <div className={styles.login}>
+            <div className={styles.deepContainer}>
+                <Icon
+                    className={styles.logo}
+                    name="deepLogo"
+                />
+                <h2 className={styles.heading}>
+                    <small>{_ts('login', 'welcomeToText')}</small><br />
+                </h2>
+            </div>
+            <div className={styles.loginFormContainer}>
+                <div className={styles.hidLinkContainer}>
+                    <a
+                        className={styles.hidLink}
+                        href={hidUrl}
+                        onClick={onHidLoginClick}
                     >
-                        <NonFieldErrors faramElement />
-                        <TextInput
-                            faramElementName="email"
-                            label={_ts('login', 'emailLabel')}
-                            placeholder={_ts('login', 'emailPlaceholder')}
-                            autoFocus
+                        <Icon
+                            className={styles.logo}
+                            name="hidLogo"
+                            alt={_ts('login', 'logInWIthHid')}
+                            draggable="false"
                         />
-                        <TextInput
-                            faramElementName="password"
-                            label={_ts('login', 'passwordLabel')}
-                            placeholder={_ts('login', 'passwordPlaceholder')}
-                            type="password"
-                        />
-                        { showReCaptcha &&
-                            <ReCaptcha
-                                setResetFunction={this.setResetRecaptchaFunction}
-                                faramElementName="recaptchaResponse"
-                                siteKey={reCaptchaSiteKey}
-                            />
-                        }
-                        <div className={styles.actionButtons}>
-                            <Link
-                                className={styles.forgotPasswordLink}
-                                to={reverseRoute(pathNames.passwordReset, {})}
-                            >
-                                {_ts('login', 'forgotPasswordText')}
-                            </Link>
-                            <PrimaryButton
-                                type="submit"
-                                pending={pending}
-                            >
-                                {_ts('login', 'loginLabel')}
-                            </PrimaryButton>
-                        </div>
-                    </Faram>
-                    <div className={styles.registerLinkContainer}>
-                        <p>
-                            {_ts('login', 'noAccountYetText')}
-                        </p>
-                        <Link
-                            className={styles.registerLink}
-                            to={reverseRoute(pathNames.register, {})}
-                        >
-                            {_ts('login', 'registerLabel')}
-                        </Link>
+                        <span>
+                            {_ts('login', 'logInWIthHid')}
+                        </span>
+                    </a>
+                    <div className={styles.orContainer}>
+                        <hr />
+                        <span className={styles.or}>
+                            {_ts('login', 'orText')}
+                        </span>
                     </div>
+                </div>
+                <Faram
+                    className={styles.loginForm}
+                    onChange={handleFaramChange}
+                    onValidationFailure={handleFaramValidationFailure}
+                    onValidationSuccess={handleFaramValidationSuccess}
+                    schema={finalSchema}
+                    value={faramValues}
+                    error={faramErrors}
+                    disabled={pending}
+                >
+                    <NonFieldErrors faramElement />
+                    <TextInput
+                        faramElementName="email"
+                        label={_ts('login', 'emailLabel')}
+                        placeholder={_ts('login', 'emailPlaceholder')}
+                        autoFocus
+                    />
+                    <TextInput
+                        faramElementName="password"
+                        label={_ts('login', 'passwordLabel')}
+                        placeholder={_ts('login', 'passwordPlaceholder')}
+                        type="password"
+                    />
+                    { showReCaptcha &&
+                        <ReCaptcha
+                            componentRef={recaptchaRef}
+                            faramElementName="recaptchaResponse"
+                            siteKey={reCaptchaSiteKey}
+                        />
+                    }
+                    <div className={styles.actionButtons}>
+                        <Link
+                            className={styles.forgotPasswordLink}
+                            to={reverseRoute(pathNames.passwordReset, {})}
+                        >
+                            {_ts('login', 'forgotPasswordText')}
+                        </Link>
+                        <PrimaryButton
+                            type="submit"
+                            pending={pending}
+                        >
+                            {_ts('login', 'loginLabel')}
+                        </PrimaryButton>
+                    </div>
+                </Faram>
+                <div className={styles.registerLinkContainer}>
+                    <p>
+                        {_ts('login', 'noAccountYetText')}
+                    </p>
+                    <Link
+                        className={styles.registerLink}
+                        to={reverseRoute(pathNames.register, {})}
+                    >
+                        {_ts('login', 'registerLabel')}
+                    </Link>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+Login.propTypes = {
+    authenticate: PropTypes.func.isRequired,
+    location: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+    login: PropTypes.func.isRequired,
+    startSiloTasks: PropTypes.func.isRequired,
+};
+
+export default connect(undefined, mapDispatchToProps)(Login);
