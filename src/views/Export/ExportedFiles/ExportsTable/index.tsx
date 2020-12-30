7305c202cb8ad9980525f8c4c11c4092b33743cb
Sameer Rai <sameerai2736@gmail.com>
2020-12-30 13:46:36 +0545

    Add tabbed layout for exports

---
+++ src/views/Export/ExportedFiles/ExportsTable/index.tsx
@@ -0,0 +1,259 @@
+import React, { useCallback, useMemo } from 'react';
+
+import {
+    compareString,
+    compareDate,
+} from '@togglecorp/fujs';
+
+import Icon from '#rscg/Icon';
+import FormattedDate from '#rscv/FormattedDate';
+import RawTable from '#rscv/RawTable';
+import TableHeader from '#rscv/TableHeader';
+import LoadingAnimation from '#rscv/LoadingAnimation';
+import DangerConfirmButton from '#rsca/ConfirmButton/DangerConfirmButton';
+
+import { mimeTypeToIconMap } from '#entities/lead';
+import _ts from '#ts';
+
+import { Export } from '#typings';
+import { Header } from '#rscv/Table';
+
+import styles from './styles.scss';
+
+interface Props {
+    exports: Export[];
+    setSelectedExport: (key: number) => void;
+    selectedExport?: number;
+    pending: boolean;
+    activeSort: string;
+    setActiveSort: (sort: string) => void;
+    handleExportDelete?: (id: number) => void;
+    deletePending?: boolean;
+    deleteExportId?: number;
+}
+
+
+const tableKeyExtractor = (d: Export) => d.id;
+
+const defaultHeaders: Header<Export>[] = [
+    {
+        key: 'mime-type',
+        label: _ts('export', 'documentTypeHeaderLabel'),
+        order: 1,
+        modifier: (row) => {
+            const icon = mimeTypeToIconMap[row.mimeType] || 'documentText';
+            const url = row.file;
+            return (
+                <div className="icon-wrapper">
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        <Icon name={icon} />
+                    </a>
+                </div>
+            );
+        },
+    },
+    {
+        key: 'exported_at',
+        label: _ts('export', 'exportedAtHeaderLabel'),
+        order: 2,
+        sortable: true,
+        comparator: (a, b) => compareDate(a.exportedAt, b.exportedAt),
+        modifier: row => (
+            <FormattedDate
+                value={row.exportedAt}
+                mode="dd-MM-yyyy hh:mm"
+            />
+        ),
+    },
+    {
+        key: 'title',
+        label: _ts('export', 'exportTitleHeaderLabel'),
+        order: 3,
+        sortable: true,
+        comparator: (a, b) => compareString(a.title, b.title),
+    },
+    {
+        key: 'status',
+        label: _ts('export', 'statusHeaderLabel'),
+        order: 4,
+        sortable: true,
+        comparator: (a, b) => (
+            compareString(a.status, b.status)
+        ),
+        modifier: (row) => {
+            if (row.status === 'pending') {
+                return _ts('export', 'pendingStatusLabel');
+            } else if (row.status === 'started') {
+                return _ts('export', 'startedStatusLabel');
+            } else if (row.status === 'failure') {
+                return _ts('export', 'errorStatusLabel');
+            } else if (row.status === 'success') {
+                return _ts('export', 'completedStatusLabel');
+            }
+            return '';
+        },
+    },
+    {
+        key: 'type',
+        label: _ts('export', 'exportTypeHeaderLabel'),
+        order: 5,
+        sortable: true,
+        comparator: (a, b) => (
+            compareString(a.type, b.type) || compareString(a.title, b.title)
+        ),
+    },
+    {
+        key: 'file',
+        label: _ts('export', 'exportDownloadHeaderLabel'),
+        order: 6,
+        modifier: (row) => {
+            if (row.status === 'started') {
+                return (
+                    <div className={styles.loadingAnimation}>
+                        <LoadingAnimation />
+                    </div>
+                );
+            } else if (row.status === 'pending') {
+                return (
+                    <div className="to-be-started">
+                        <Icon
+                            name="history"
+                            title={_ts('export', 'toBeStartedTitle')}
+                        />
+                    </div>
+                );
+            } else if (row.status === 'failure' || !row.file) {
+                return (
+                    <div className="file-error">
+                        <Icon name="error" />
+                    </div>
+                );
+            }
+            return (
+                <a
+                    href={row.file}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="file-download"
+                >
+                    <Icon name="download" />
+                </a>
+            );
+        },
+    },
+];
+
+function ExportsTable(props: Props) {
+    const {
+        exports,
+        setSelectedExport,
+        selectedExport,
+        pending,
+        activeSort,
+        setActiveSort,
+        handleExportDelete,
+        deleteExportId,
+        deletePending,
+    } = props;
+
+    const headers: Header<Export>[] = useMemo(() => {
+        if (handleExportDelete) {
+            return [
+                ...defaultHeaders,
+                {
+                    key: 'action',
+                    label: _ts('export', 'exportActionsLabel'),
+                    order: 6,
+                    sortable: false,
+                    modifier: row => (
+                        <DangerConfirmButton
+                            onClick={() => handleExportDelete(row.id)}
+                            iconName="delete"
+                            disabled={row.id === deleteExportId && deletePending}
+                            title={_ts('export', 'exportDeleteLabel')}
+                            confirmationMessage={_ts('export', 'exportDeleteConfirmationMessage')}
+                            transparent
+                        />
+                    ),
+                },
+            ];
+        }
+        return defaultHeaders;
+    }, [deleteExportId, deletePending, handleExportDelete]);
+
+    const dataModifier = useCallback(
+        (data, columnKey) => {
+            const header = headers.find(d => d.key === columnKey);
+            if (header?.modifier) {
+                return header.modifier(data);
+            }
+            return data[columnKey];
+        }, [headers],
+    );
+
+    const headerModifier = useCallback((headerData) => {
+        let sortOrder: 'asc' | 'dsc' | undefined;
+
+        if (activeSort === headerData.key) {
+            sortOrder = 'asc';
+        } else if (activeSort === `-${headerData.key}`) {
+            sortOrder = 'dsc';
+        }
+
+        return (
+            <TableHeader
+                label={headerData.label}
+                sortOrder={sortOrder}
+                sortable={headerData.sortable}
+            />
+        );
+    }, [activeSort]);
+
+    const handleTableHeaderClick = useCallback(
+        (key) => {
+            const headerData = headers.find(h => h.key === key);
+            // prevent click on 'actions' column
+            if (!headerData || !headerData.sortable) {
+                return;
+            }
+
+            let tmpActiveSort = activeSort;
+
+            const isAsc = tmpActiveSort.charAt(0) !== '-';
+
+            const isCurrentHeaderSorted = tmpActiveSort === key
+                || (tmpActiveSort.substr(1) === key && !isAsc);
+
+            if (isCurrentHeaderSorted) {
+                tmpActiveSort = isAsc ? `-${key}` : key;
+            } else {
+                tmpActiveSort = headerData.defaultSortOrder === 'dsc' ? `-${key}` : key;
+            }
+
+            setActiveSort(tmpActiveSort);
+        }, [headers, activeSort, setActiveSort],
+    );
+
+    const handleBodyClick = useCallback((key, column) => {
+        if (column !== 'action') {
+            setSelectedExport(key);
+        }
+    }, [setSelectedExport]);
+
+    return (
+        <RawTable
+            className={styles.table}
+            data={exports}
+            dataModifier={dataModifier}
+            headerModifier={headerModifier}
+            headers={headers}
+            onHeaderClick={handleTableHeaderClick}
+            onBodyClick={handleBodyClick}
+            keySelector={tableKeyExtractor}
+            pending={pending}
+            highlightRowKey={selectedExport}
+        />
+    );
+}
+
+export default ExportsTable;
---
+++ src/views/Export/ExportedFiles/index.tsx
@@ -0,0 +1,236 @@
+import React, { useCallback, useState, useMemo } from 'react';
+
+import ScrollTabs from '#rscv/ScrollTabs';
+import TabTitle from '#components/general/TabTitle';
+import MultiViewContainer from '#rscv/MultiViewContainer';
+import Page from '#rscv/Page';
+import Pager from '#rscv/Pager';
+
+import {
+    MultiResponse,
+    Export,
+    ExportStatus,
+} from '#typings';
+import notify from '#notify';
+import _ts from '#ts';
+import ExportPreview from '#components/other/ExportPreview';
+import useRequest from '#utils/request';
+
+import ExportsTable from './ExportsTable';
+import styles from './styles.scss';
+
+interface Props {
+    projectId: number;
+}
+
+const maxItemsPerPage = 5;
+
+type TabElement = 'pending' | 'recent' | 'archived';
+const tabs: { [key in TabElement]: string} = {
+    pending: 'Downloads in progres',
+    recent: 'Recent downloads',
+    archived: 'Archive',
+};
+
+const tabExportStatus: { [key in TabElement]: ExportStatus} = {
+    pending: 'pending',
+    recent: 'success',
+    archived: 'success',
+};
+
+function ExportedFiles(props: Props) {
+    const { projectId } = props;
+
+    const [activeTab, setActiveTab] = useState<TabElement>('pending');
+    const [activeSort, setActiveSort] = useState('-exported_at');
+    const [activePage, setActivePage] = useState(1);
+    const [userExports, setUserExports] = useState<Export[]>([]);
+    const [exportCount, setExportCount] = useState<number>(0);
+    const [selectedExport, setSelectedExport] = useState<number>();
+    const [deleteExportId, setDeleteExportId] = useState<number>();
+
+    const status = useMemo(() => tabExportStatus[activeTab], [activeTab]);
+    const isArchived = useMemo(() => activeTab === 'archived', [activeTab]);
+
+    const [
+        pending,
+        ,
+        ,
+        getExport,
+    ] = useRequest<MultiResponse<Export>>({
+        url: 'server://exports/',
+        query: {
+            project: projectId,
+            ordering: activeSort,
+            is_preview: false,
+            is_archived: isArchived,
+            status,
+            offset: (activePage - 1) * maxItemsPerPage,
+            limit: maxItemsPerPage,
+        },
+        method: 'GET',
+        autoTrigger: true,
+        schemaName: 'userExportsGetResponse',
+        onSuccess: (response) => {
+            setUserExports(response.results);
+            setExportCount(response.count);
+        },
+        onFailure: () => {
+            notify.send({
+                title: _ts('export', 'userExportsTitle'),
+                type: notify.type.ERROR,
+                message: _ts('export', 'userExportsFailure'),
+                duration: notify.duration.MEDIUM,
+            });
+        },
+    });
+
+    const [
+        deletePending,
+        ,
+        ,
+        deleteExport,
+    ] = useRequest({
+        url: `server://exports/${deleteExportId}/`,
+        method: 'DELETE',
+        onSuccess: () => {
+            getExport();
+            setExportCount(oldCount => oldCount - 1);
+            if (deleteExportId === selectedExport) {
+                setSelectedExport(undefined);
+            }
+            notify.send({
+                title: _ts('export', 'userExportsTitle'),
+                type: notify.type.SUCCESS,
+                message: _ts('export', 'deleteExportSuccess'),
+                duration: notify.duration.MEDIUM,
+            });
+        },
+        autoTrigger: false,
+        onFailure: () => {
+            notify.send({
+                title: _ts('export', 'userExportsTitle'),
+                type: notify.type.ERROR,
+                message: _ts('export', 'deleteExportFailure'),
+                duration: notify.duration.MEDIUM,
+            });
+        },
+    });
+
+    const handleExportDelete = useCallback((id) => {
+        setDeleteExportId(id);
+        deleteExport();
+    }, [deleteExport]);
+
+    const handleTabChange = useCallback((tab: TabElement) => {
+        setSelectedExport(undefined);
+        setActivePage(1);
+        setActiveTab(tab);
+    }, []);
+
+    const views = useMemo(() => (
+        {
+            pending: {
+                component: () => (
+                    <ExportsTable
+                        exports={userExports}
+                        pending={pending}
+                        selectedExport={selectedExport}
+                        setSelectedExport={setSelectedExport}
+                        activeSort={activeSort}
+                        setActiveSort={setActiveSort}
+                    />
+                ),
+                lazyMount: true,
+                wrapContainer: true,
+            },
+            recent: {
+                component: () => (
+                    <ExportsTable
+                        exports={userExports}
+                        pending={pending}
+                        selectedExport={selectedExport}
+                        setSelectedExport={setSelectedExport}
+                        activeSort={activeSort}
+                        setActiveSort={setActiveSort}
+                        deleteExportId={deleteExportId}
+                        handleExportDelete={handleExportDelete}
+                        deletePending={deletePending}
+                    />
+                ),
+                lazyMount: true,
+                wrapContainer: true,
+            },
+            archived: {
+                component: () => (
+                    <ExportsTable
+                        exports={userExports}
+                        pending={pending}
+                        selectedExport={selectedExport}
+                        setSelectedExport={setSelectedExport}
+                        activeSort={activeSort}
+                        setActiveSort={setActiveSort}
+                    />
+                ),
+                lazyMount: true,
+                wrapContainer: true,
+            },
+        }
+    ), [
+        pending,
+        selectedExport,
+        userExports,
+        activeSort,
+        deleteExportId,
+        deletePending,
+        handleExportDelete,
+    ]);
+
+    const tabRendererParams = useCallback((_: TabElement, title: string) => ({
+        title,
+    }), []);
+
+    return (
+        <Page
+            className={styles.exportedFiles}
+            headerClassName={styles.header}
+            header={
+                <ScrollTabs
+                    tabs={tabs}
+                    active={activeTab}
+                    renderer={TabTitle}
+                    onClick={handleTabChange}
+                    rendererParams={tabRendererParams}
+                />
+            }
+            mainContentClassName={styles.mainContent}
+            mainContent={
+                <>
+                    <MultiViewContainer
+                        containerClassName={styles.left}
+                        views={views}
+                        active={activeTab}
+                    />
+                    <ExportPreview
+                        key={selectedExport}
+                        className={styles.preview}
+                        exportId={selectedExport}
+                    />
+                </>
+            }
+            footerClassName={styles.footer}
+            footer={
+                <Pager
+                    activePage={activePage}
+                    className={styles.pager}
+                    itemsCount={exportCount}
+                    maxItemsPerPage={maxItemsPerPage}
+                    onPageClick={setActivePage}
+                    showItemsPerPageChange={false}
+                />
+            }
+        />
+    );
+}
+
+export default ExportedFiles;
