4108ebe495c41b5e0ec89ce463bd7e70a6a44bdf
Sameer Rai <sameerai2736@gmail.com>
2021-02-23 16:28:25 +0545

    Create EntryReviewModal with draggable feature

---
+++ src/hooks/useDragMove.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect, useCallback, SyntheticEvent } from 'react';
+
+interface Props {
+    onPointerDown?: (e: PointerEvent) => void;
+    onPointerMove?: (e: PointerEvent) => void;
+    onPointerUp?: (e: PointerEvent) => void;
+    onDragMove: (e: PointerEvent) => void;
+}
+function useDragMove(props: Props) {
+    const { onPointerDown, onPointerUp, onPointerMove, onDragMove } = props;
+    const [isDragging, setIsDragging] = useState<boolean>(false);
+
+    const handlePointerDown = useCallback((e) => {
+        setIsDragging(true);
+        if (onPointerDown) {
+            onPointerDown(e);
+        }
+    }, [onPointerDown]);
+
+    const handlePointerUp = useCallback((e) => {
+        setIsDragging(false);
+        if (onPointerUp) {
+            onPointerUp(e);
+        }
+    }, [onPointerUp]);
+
+    const handlePointerMove = useCallback((e) => {
+        if (isDragging) {
+            onDragMove(e);
+        }
+        if (onPointerMove) {
+            onPointerMove(e);
+        }
+    }, [onDragMove, onPointerMove, isDragging]);
+
+    useEffect(() => {
+        window.addEventListener('pointermove', handlePointerMove);
+        window.addEventListener('pointerup', handlePointerUp);
+        return () => {
+            window.removeEventListener('pointermove', handlePointerMove);
+            window.removeEventListener('pointerup', handlePointerUp);
+        };
+    }, [handlePointerMove, handlePointerUp]);
+
+    return { isDragging, handlePointerDown };
+}
+
+export default useDragMove;
