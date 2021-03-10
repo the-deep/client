678e7b0b190afee4e82ea7ffae3c7b708f8cf35b
Subina <subina@togglecorp.com>
2021-03-10 20:13:47 +0545

    Typecheck and other comment fixes

--- src/typings/analysisModule.tsx
+++ src/typings/analysisModule.tsx
@@ -5,3 +5,13 @@ export interface AnalysisElement extends BasicElement {
 
 export interface PillarAnalysisElement extends BasicElement {
 }
+
+export interface AnalysisPillars {
+    id: number;
+    analysis: number;
+    assigneeName: string;
+    title: string;
+    mainStatement?: string;
+    informationGap?: string;
+    filters?: unknown; // To be assigned a type when filters are passed from API
+}
