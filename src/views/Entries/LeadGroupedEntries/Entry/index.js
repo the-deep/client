2bab89cc69cc094ebdde09b8d5e6ae1073e5c6f0
frozenhelium <fren.ankit@gmail.com>
2021-03-12 11:11:41 +0545

    Fix widget prop in entry review modal

--- src/views/Entries/LeadGroupedEntries/Entry/index.js
+++ src/views/Entries/LeadGroupedEntries/Entry/index.js
@@ -294,8 +294,7 @@ export default class Entry extends React.PureComponent {
                 <Widget
                     widget={widget}
                     widgetName={widgetId}
-                    wid
-                    getType={VIEW.list}
+                    widgetType={VIEW.list}
                 />
             );
         }
