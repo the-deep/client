8d4d214971f0630c18a5e43c26a12f486035a773
Sameer Rai <sameerai2736@gmail.com>
2021-03-15 16:11:09 +0545

    Add entry review comment edit

--- src/typings/entry.ts
+++ src/typings/entry.ts
@@ -154,6 +154,7 @@ export interface EntryComment {
     commentTypeDisplay: string;
     commentType: number;
     createdBy: number;
+    createdAt: string;
     entry: number;
     mentionedUsers: number[];
 }
