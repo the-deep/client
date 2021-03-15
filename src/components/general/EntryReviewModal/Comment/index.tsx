711b0c658c7aa3818d7a32c488ab69b4a904fd82
Sameer Rai <sameerai2736@gmail.com>
2021-03-15 16:11:09 +0545

    Add entry review comment edit

--- src/components/general/EntryReviewModal/Comment/index.tsx
+++ src/components/general/EntryReviewModal/Comment/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { _cs } from '@togglecorp/fujs';
 import ReactMarkdown from 'react-markdown';
 
 import FormattedDate from '#rscv/FormattedDate';
 import DisplayPicture from '#components/viewer/DisplayPicture';
 import CommaSeparateItems from '#components/viewer/CommaSeparateItems';
+import PrimaryButton from '#rsca/Button/PrimaryButton';
 
 import { EntryComment } from '#typings';
 import _ts from '#ts';
 
+import CommentForm from './CommentForm';
 import styles from './styles.scss';
 
 interface Props {
     className?: string;
     comment: EntryComment;
+    activeUser: {
+        userId: number;
+    };
 }
 
 const commentTypeToTextMap: {[id: number]: string} = {
@@ -27,15 +32,38 @@ const commentTypeToTextMap: {[id: number]: string} = {
 function Comment(props: Props) {
     const {
         className,
-        comment: {
-            textHistory,
-            createdByDetail,
-            commentType,
-            mentionedUsersDetail,
-        },
+        comment: commentFromProps,
+        activeUser,
     } = props;
 
+    const [comment, setComment] = useState<EntryComment>(commentFromProps);
+    const [editMode, setEditMode] = useState<boolean>(false);
+
+    const {
+        textHistory,
+        createdByDetail,
+        commentType,
+        mentionedUsersDetail,
+        createdAt,
+    } = comment;
     const [latest] = textHistory;
+
+    const isEditable = useMemo(() =>
+        activeUser.userId === createdByDetail.id && latest && !editMode,
+    [activeUser, createdByDetail, latest, editMode]);
+
+    const handleEditClick = useCallback(() => {
+        setEditMode(true);
+    }, []);
+
+    const handleCancel = useCallback(() => {
+        setEditMode(false);
+    }, []);
+
+    const handleSuccess = useCallback((value: EntryComment) => {
+        setComment(value);
+        setEditMode(false);
+    }, []);
 
     return (
         <div
@@ -48,29 +76,47 @@ function Comment(props: Props) {
             <div
                 className={styles.content}
             >
-                <span className={styles.detail}>
-                    <span className={styles.name}>{createdByDetail.name}</span>
-                    <span>
-                        &nbsp;
-                        {_ts('entryReview', 'commentType', { commentType: commentTypeToTextMap[commentType] })}
-                        &nbsp;
-                        <FormattedDate
-                            value={latest.createdAt}
-                            mode="dd-MMM-yyyy"
-                        />
-                        &nbsp;
-                        {mentionedUsersDetail.length > 0 && _ts('entryReview', 'assignedItTo')}
-                        &nbsp;
+                <div className={styles.heading}>
+                    <span className={styles.detail}>
+                        <span className={styles.name}>{createdByDetail.name}</span>
+                        <span>
+                            &nbsp;
+                            {_ts('entryReview', 'commentType', { commentType: commentTypeToTextMap[commentType] })}
+                            &nbsp;
+                            <FormattedDate
+                                value={latest ? latest.createdAt : createdAt}
+                                mode="dd-MMM-yyyy"
+                            />
+                            &nbsp;
+                            {mentionedUsersDetail.length > 0 && _ts('entryReview', 'assignedItTo')}
+                            &nbsp;
+                        </span>
+                        <span>
+                            <CommaSeparateItems
+                                items={mentionedUsersDetail}
+                            />
+                        </span>
                     </span>
-                    <span>
-                        <CommaSeparateItems
-                            items={mentionedUsersDetail}
+                    {isEditable && (
+                        <PrimaryButton
+                            onClick={handleEditClick}
+                            transparent
+                            title={_ts('entryReview', 'editComment')}
+                            iconName="edit"
                         />
-                    </span>
-                </span>
-                <ReactMarkdown
-                    source={latest.text}
-                />
+                    )}
+                </div>
+                {editMode ? (
+                    <CommentForm
+                        comment={comment}
+                        onEditSuccess={handleSuccess}
+                        onEditCancel={handleCancel}
+                    />
+                ) : (
+                    <ReactMarkdown
+                        source={latest ? latest.text : undefined}
+                    />
+                )}
             </div>
         </div>
     );
