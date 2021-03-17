bb78558e314386d4cc7f5fd74382fbd192bda097
Subina <subina@togglecorp.com>
2021-03-17 15:33:14 +0545

    Add Date Filter to Analysis Module

--- src/typings/analysisModule.tsx
+++ src/typings/analysisModule.tsx
@@ -1,7 +1,6 @@
 import { BasicElement } from './';
 
 export interface AnalysisElement extends BasicElement {
-<<<<<<< HEAD
     title: string;
     teamLead: number;
     teamLeadName: string;
@@ -9,13 +8,10 @@ export interface AnalysisElement extends BasicElement {
     startDate?: string;
     endDate?: string;
     createdOn?: string;
-=======
->>>>>>> bc57cab00 (Feature/analysis module setup 2 (#1399))
 }
 
 export interface PillarAnalysisElement extends BasicElement {
 }
-<<<<<<< HEAD
 
 export interface AnalysisPillars {
     id: number;
@@ -27,5 +23,3 @@ export interface AnalysisPillars {
     informationGap?: string;
     filters?: unknown; // To be assigned a type when filters are passed from API
 }
-=======
->>>>>>> bc57cab00 (Feature/analysis module setup 2 (#1399))
