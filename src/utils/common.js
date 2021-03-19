a22547ad2fbf2dfab9cbceeae10046d912affb17
Aditya Khatri <aditya.khatri@togglecorp.com>
2021-03-19 11:52:26 +0545

    Fix typings

--- src/utils/common.js
+++ src/utils/common.js
@@ -246,18 +246,3 @@ export function hasKey(data, key, keySelector, childrenSelector) {
     }
     return false;
 }
-
-export const shortMonthNamesMap = {
-    0: 'Jan',
-    1: 'Feb',
-    2: 'Mar',
-    3: 'Apr',
-    4: 'May',
-    5: 'Jun',
-    6: 'Jul',
-    7: 'Aug',
-    8: 'Sep',
-    9: 'Oct',
-    10: 'Nov',
-    11: 'Dec',
-};
