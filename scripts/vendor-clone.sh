9e11fb224133f8bb13a07b0ea2aa68a0df6fba04
thenav56 <ayernavin@togglecorp.com>
2021-04-05 10:55:46 +0545

    Use https to clone vendor if ssh is not configured

--- scripts/vendor-clone.sh
+++ scripts/vendor-clone.sh
@@ -4,6 +4,12 @@
 if [ -d "src/vendor/react-store" ]; then
     echo "Skipping react-store (already cloned)"
 else
+    ssh -T git@github.com
+    ssh_error_code=$?
     echo "Cloning react-store to src/vendor/react-store"
-    git clone git@github.com:toggle-corp/react-store.git src/vendor/react-store
+    clone_url='git@github.com:toggle-corp/react-store.git'
+    if [ $ssh_error_code -eq 255 ]; then
+        clone_url='https://github.com/toggle-corp/react-store.git'
+    fi
+    git clone $clone_url src/vendor/react-store
 fi
