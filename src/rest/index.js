30b6d0e144f3616f08a4a3fbedfecb50bd49b5ec
tnagorra <weathermist@gmail.com>
2021-07-08 14:49:23 +0545

    Add response error to form error transformer
    - Fix filter apply action for analysis pillar
    - Set server error from analysis pillar update
    - Pass response errors to onFailure

--- src/rest/index.js
+++ src/rest/index.js
@@ -4,6 +4,16 @@ import {
     commonHeaderForGet,
     commonHeaderForPost,
 } from '#config/rest';
+import {
+    listToMap,
+    mapToMap,
+    doesObjectHaveNoData,
+    isNotDefined,
+    isDefined,
+} from '@togglecorp/fujs';
+import {
+    internal,
+} from '@togglecorp/toggle-form';
 
 export * from './analysisFramework';
 export * from './assessmentRegistry';
@@ -81,3 +91,98 @@ export const alterResponseErrorToFaramError = (errors) => {
 export const alterAndCombineResponseError = errors => (
     Object.values(alterResponseErrorToFaramError(errors))
 );
+
+// NOTE: the functions below are generic transformers to
+// transform server error to form error
+
+function transform(formSchema, responseValue, responseError) {
+    if (!formSchema || !responseError) {
+        return undefined;
+    }
+
+    if (Array.isArray(formSchema)) {
+        if (responseError.length <= 0) {
+            return undefined;
+        }
+        return responseError.join(' ');
+    }
+
+    if (formSchema.member && formSchema.keySelector) {
+        // FIXME: there may be a case where the error can be similar to leaf
+
+        const errorList = responseError.map((error, i) => {
+            if (!error) {
+                return undefined;
+            }
+
+            const valueItem = responseValue?.[i];
+            if (isNotDefined(valueItem)) {
+                return undefined;
+            }
+
+            const key = formSchema.keySelector(valueItem);
+            const localSchema = formSchema.member(valueItem);
+
+            const transformedValue = transform(
+                localSchema,
+                valueItem,
+                error,
+            );
+            return { key, transformedValue };
+        }).filter(isDefined);
+
+        const errorMap = listToMap(
+            errorList,
+            error => error.key,
+            error => error.transformedValue,
+        );
+
+        return doesObjectHaveNoData(errorMap)
+            ? undefined
+            : errorMap;
+    }
+
+    if (formSchema.fields) {
+        const objectSchema = formSchema.fields(responseValue);
+
+        // FIXME: how does this map with array error?
+        const {
+            nonFieldErrors,
+            ...otherErrors
+        } = responseError;
+
+        // FIXME: there may be a case where the error can be similar to leaf
+
+        const errorMap = mapToMap(
+            otherErrors,
+            key => key,
+            (err, key) => {
+                const localSchema = objectSchema?.[key];
+                const valueItem = responseValue?.[key];
+
+                const transformedValue = transform(
+                    localSchema,
+                    valueItem,
+                    err,
+                );
+                return transformedValue;
+            },
+        );
+        if (nonFieldErrors) {
+            errorMap[internal] = nonFieldErrors.join(' ');
+        }
+        return doesObjectHaveNoData(errorMap)
+            ? undefined
+            : errorMap;
+    }
+    return undefined;
+}
+
+export function transformErrorToToggleFormError(formSchema, obj, responseError) {
+    try {
+        return transform(formSchema, obj, responseError);
+    } catch {
+        console.error('Error while transforming server response');
+        return undefined;
+    }
+}
