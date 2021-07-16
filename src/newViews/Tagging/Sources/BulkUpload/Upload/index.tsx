50a40b74fa40b1efd84cc4bf9fe6204905f9554a
Sameer Rai <sameerai2736@gmail.com>
2021-07-16 10:09:31 +0545

    Add GoogleDrivePicker component

--- src/newViews/Tagging/Sources/BulkUpload/Upload/index.tsx
+++ src/newViews/Tagging/Sources/BulkUpload/Upload/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import { _cs } from '@togglecorp/fujs';
+import { FaGoogleDrive } from 'react-icons/fa';
 import {
     Container,
 } from '@the-deep/deep-ui';
 import _ts from '#ts';
+import {
+    googleDriveClientId,
+    googleDriveDeveloperKey,
+} from '#config/google-drive';
+import {
+    supportedGoogleDriveMimeTypes,
+} from '../utils';
+
+import GoogleDrivePicker from './GoogleDrivePicker';
+
+import styles from './styles.scss';
 
 interface Props {
     className?: string;
@@ -14,12 +26,32 @@ function Upload(props: Props) {
         className,
     } = props;
 
+    const handleChange = (response: google.picker.ResponseObject) => {
+        console.warn('values', response.docs);
+    };
+
+    const mimeTypes = supportedGoogleDriveMimeTypes.join(' ');
     return (
         <Container
-            className={className}
+            className={_cs(className, styles.upload)}
             heading={_ts('bulkUpload', 'uploadFilesTitle')}
             sub
-        />
+            contentClassName={styles.content}
+        >
+            <GoogleDrivePicker
+                className={styles.googlePicker}
+                clientId={googleDriveClientId}
+                developerKey={googleDriveDeveloperKey}
+                onChange={handleChange}
+                mimeTypes={mimeTypes}
+                icons={<FaGoogleDrive />}
+                iconsClassName={styles.icon}
+                multiSelect
+                navHidden
+            >
+                {_ts('bulkUpload', 'googleDriveLabel')}
+            </GoogleDrivePicker>
+        </Container>
     );
 }
 
---
+++ src/newViews/Tagging/Sources/BulkUpload/utils.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/prefer-default-export
+export const supportedGoogleDriveMimeTypes = [
+    'application/json', 'application/xml', 'application/msword',
+    'application/rtf', 'text/plain', 'font/otf', 'application/pdf',
+    'application/vnd.ms-powerpoint', 'application/vnd.ms-excel',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    'application/wps-office.xlsx',
+    'application/vnd.oasis.opendocument.spreadsheet',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    'image/fig',
+    'image/jpeg',
+    'image/png',
+    'text/csv',
+];
