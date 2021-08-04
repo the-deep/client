39004711c4c6bfddcf29a2d75e9b50d76ccbe946
frozenhelium <fren.ankit@gmail.com>
2021-08-04 15:05:15 +0545

    Fix linting issuess

--- app/types/common.tsx
+++ app/types/common.tsx
@@ -1,8 +1,3 @@
-import {
-    NewProps,
-    ClientAttributes,
-} from '@togglecorp/react-rest-request';
-
 // Basic
 
 export interface BasicElement {
@@ -16,52 +11,11 @@ export interface KeyValueElement {
     value: string;
 }
 
-// Redux
-
-export interface AppState {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    domainData: any;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    siloDomainData: any;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    route: any;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    auth: any;
-}
-
-export interface AppProps {
-}
-
-// Request
-
-export type Requests<Props, Params> = {[key: string]: ClientAttributes<Props, Params>}
-
-export type AddRequestProps<Props, Params> = NewProps<Props, Params>;
-
 export interface MultiResponse<T> {
     count: number;
     next?: string;
     previous?: string;
     results: T[];
-}
-
-export interface ViewComponent<T> {
-    component: React.ComponentType<T>;
-    rendererParams?: () => T;
-    wrapContainer?: boolean;
-    mount?: boolean;
-    lazyMount?: boolean;
-}
-
-export interface Permission {
-    create?: boolean;
-    delete?: boolean;
-    view?: boolean;
-    modify?: boolean;
-}
-
-export interface Permissions {
-    setupPermissions: Permission;
 }
 
 export type NullableField<T, K extends keyof T> = {
@@ -86,9 +40,4 @@ export interface BooleanKeyValueElement {
 
 export interface FaramErrors {
     [key: string]: string | undefined | string [] | FaramErrors;
-}
-
-export interface NavbarContextProps {
-    parentNode: HTMLDivElement | null | undefined;
-    setParentNode: (node: HTMLDivElement | null | undefined) => void;
 }
