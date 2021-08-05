1cd10e6acf745291867ef4419f4751dd4123bb5b
tnagorra <weathermist@gmail.com>
2021-08-05 17:15:13 +0545

    Add select input to switch project
    - Create project search select input

--- app/views/Tagging/index.tsx
+++ app/views/Tagging/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useMemo, useState } from 'react';
 import {
     Switch,
     Route,
@@ -7,8 +7,10 @@ import {
 } from 'react-router-dom';
 import { _cs } from '@togglecorp/fujs';
 
+import ProjectSwitcher from '#components/ProjectSwitcher';
 import PreloadMessage from '#base/components/PreloadMessage';
-import SubNavbar from '#components/SubNavbar';
+import NavbarContext from '#components/SubNavbar/context';
+import SubNavbar, { Icons } from '#components/SubNavbar';
 import ProjectContext from '#base/context/ProjectContext';
 import SmartNavLink from '#base/components/SmartNavLink';
 import routes from '#base/configs/routes';
@@ -22,77 +24,119 @@ interface Props {
 function Tagging(props: Props) {
     const { className } = props;
     const { project } = React.useContext(ProjectContext);
+
     const defaultRoute = generatePath(routes.sources.path, { projectId: project?.id });
 
+    const [iconsNode, setIconsNode] = useState<Element | null | undefined>();
+    const [actionsNode, setActionsNode] = useState<Element | null | undefined>();
+
+    const navbarContextValue = useMemo(
+        () => ({
+            iconsNode,
+            actionsNode,
+            setIconsNode,
+            setActionsNode,
+        }),
+        [iconsNode, actionsNode],
+    );
+
     return (
-        <div className={_cs(styles.tagging, className)}>
-            <SubNavbar
-                className={styles.subNavbar}
-            >
-                <SmartNavLink
-                    exact
-                    route={routes.sources}
-                    className={styles.link}
-                />
-                <SmartNavLink
-                    exact
-                    route={routes.dashboard}
-                    className={styles.link}
-                />
-                <SmartNavLink
-                    exact
-                    route={routes.export}
-                    className={styles.link}
-                />
-            </SubNavbar>
-            <Suspense
-                fallback={(
-                    <PreloadMessage
-                        className={styles.childView}
-                        content="Loading page..."
-                    />
-                )}
-            >
-                <Switch>
-                    <Route
+        <NavbarContext.Provider value={navbarContextValue}>
+            <div className={_cs(styles.tagging, className)}>
+                <SubNavbar
+                    className={styles.subNavbar}
+                >
+                    <SmartNavLink
                         exact
-                        path={routes.tagging.path}
-                    >
-                        <Redirect to={defaultRoute} />
-                    </Route>
+                        route={routes.sources}
+                        className={styles.link}
+                    />
+                    <SmartNavLink
+                        exact
+                        route={routes.dashboard}
+                        className={styles.link}
+                    />
+                    <SmartNavLink
+                        exact
+                        route={routes.export}
+                        className={styles.link}
+                    />
+                </SubNavbar>
+                <Switch>
                     <Route
                         exact
                         path={routes.sources.path}
                     >
-                        {routes.sources.load({ className: styles.childView })}
+                        <Icons>
+                            <ProjectSwitcher />
+                        </Icons>
                     </Route>
                     <Route
                         exact
                         path={routes.dashboard.path}
                     >
-                        {routes.dashboard.load({ className: styles.childView })}
+                        <Icons>
+                            <ProjectSwitcher />
+                        </Icons>
                     </Route>
                     <Route
                         exact
                         path={routes.export.path}
                     >
-                        {routes.export.load({ className: styles.childView })}
-                    </Route>
-                    <Route
-                        exact
-                        path={routes.taggingFlow.path}
-                    >
-                        {routes.taggingFlow.load({ className: styles.childView })}
-                    </Route>
-                    <Route
-                        exact
-                        path={routes.fourHundredFour.path}
-                    >
-                        {routes.fourHundredFour.load({ className: styles.childView })}
+                        <Icons>
+                            <ProjectSwitcher />
+                        </Icons>
                     </Route>
                 </Switch>
-            </Suspense>
-        </div>
+                <Suspense
+                    fallback={(
+                        <PreloadMessage
+                            className={styles.childView}
+                            content="Loading page..."
+                        />
+                    )}
+                >
+                    <Switch>
+                        <Route
+                            exact
+                            path={routes.tagging.path}
+                        >
+                            <Redirect to={defaultRoute} />
+                        </Route>
+                        <Route
+                            exact
+                            path={routes.sources.path}
+                        >
+                            {routes.sources.load({ className: styles.childView })}
+                        </Route>
+                        <Route
+                            exact
+                            path={routes.dashboard.path}
+                        >
+                            {routes.dashboard.load({ className: styles.childView })}
+                        </Route>
+                        <Route
+                            exact
+                            path={routes.export.path}
+                        >
+                            {routes.export.load({ className: styles.childView })}
+                        </Route>
+                        <Route
+                            exact
+                            path={routes.taggingFlow.path}
+                        >
+                            {routes.taggingFlow.load({ className: styles.childView })}
+                        </Route>
+                        <Route
+                            exact
+                            path={routes.fourHundredFour.path}
+                        >
+                            {routes.fourHundredFour.load({ className: styles.childView })}
+                        </Route>
+                    </Switch>
+                </Suspense>
+            </div>
+        </NavbarContext.Provider>
     );
 }
 
