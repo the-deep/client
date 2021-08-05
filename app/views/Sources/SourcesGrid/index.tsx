c33e4dfd4cc31fce7d89cc19b1a56e1494d85c5d
frozenhelium <fren.ankit@gmail.com>
2021-08-05 11:55:37 +0545

    Add mock data for entry grid view

--- app/views/Sources/SourcesGrid/index.tsx
+++ app/views/Sources/SourcesGrid/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import { _cs } from '@togglecorp/fujs';
 
+import EntryCard from './EntryCard';
 import styles from './styles.css';
+
+const entries = [
+    {
+        leadTitle: 'The standard Lorem Ipsum passage, used since the 1500s',
+        leadCreatedOn: '2020-10-12',
+        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+        leadTitle: 'The standard Lorem Ipsum passage, used since the 1500s',
+        leadCreatedOn: '2020-10-12',
+        excerpt: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.',
+    },
+    {
+        leadTitle: 'The standard Lorem Ipsum passage, used since the 1500s',
+        leadCreatedOn: '2020-10-12',
+        excerpt: 'Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?',
+
+    },
+    {
+        leadTitle: 'Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC',
+        leadCreatedOn: '2021-03-11',
+        excerpt: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+    },
+];
 
 interface Props {
     className?: string;
@@ -12,7 +37,13 @@ function SourcesGrid(props: Props) {
 
     return (
         <div className={_cs(styles.sourcesGrid, className)}>
-            Grid view
+            {entries.map((entry) => (
+                <EntryCard
+                    leadTitle={entry.leadTitle}
+                    leadCreatedOn={entry.leadCreatedOn}
+                    excerpt={entry.excerpt}
+                />
+            ))}
         </div>
     );
 }
