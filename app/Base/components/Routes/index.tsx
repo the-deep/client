90a15ba66b6afe40bb00d3291d0ae0a566d9309c
frozenhelium <fren.ankit@gmail.com>
2021-08-05 13:19:51 +0545

    Add better styling mechanism

--- app/Base/components/Routes/index.tsx
+++ app/Base/components/Routes/index.tsx
@@ -3,55 +3,61 @@ import { Switch, Route } from 'react-router-dom';
 
 import routes from '#base/configs/routes';
 
-function Routes() {
+interface Props {
+    className?: string;
+}
+
+function Routes(props: Props) {
+    const { className } = props;
+
     return (
         <Switch>
             <Route
                 exact
                 path={routes.home.path}
             >
-                {routes.home.load({})}
+                {routes.home.load({ className })}
             </Route>
             <Route
                 exact
                 path={routes.explore.path}
             >
-                {routes.explore.load({})}
+                {routes.explore.load({ className })}
             </Route>
             <Route
                 path={routes.project.path}
             >
-                {routes.project.load({})}
+                {routes.project.load({ className })}
             </Route>
             <Route
                 exact
                 path={routes.signIn.path}
             >
-                {routes.signIn.load({})}
+                {routes.signIn.load({ className })}
             </Route>
             <Route
                 exact
                 path={routes.signUp.path}
             >
-                {routes.signUp.load({})}
+                {routes.signUp.load({ className })}
             </Route>
             <Route
                 exact
                 path={routes.forgetPassword.path}
             >
-                {routes.forgetPassword.load}
+                {routes.forgetPassword.load({ className })}
             </Route>
             <Route
                 exact
                 path={routes.resetPassword.path}
             >
-                {routes.resetPassword.load({})}
+                {routes.resetPassword.load({ className })}
             </Route>
             <Route
                 exact
                 path={routes.fourHundredFour.path}
             >
-                {routes.fourHundredFour.load({})}
+                {routes.fourHundredFour.load({ className })}
             </Route>
         </Switch>
     );
