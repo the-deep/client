ad917e964b0b85364c17d406705cd33fc199c393
frozenhelium <fren.ankit@gmail.com>
2021-08-05 23:29:05 +0545

    Add source grid view

--- app/views/Sources/SourcesGrid/EntryCard/index.tsx
+++ app/views/Sources/SourcesGrid/EntryCard/index.tsx
@@ -1,44 +1,167 @@
 import React from 'react';
 import { _cs } from '@togglecorp/fujs';
 import {
-    Card,
-    Heading,
+    IoPencil,
+    IoTrash,
+    IoClose,
+    IoChatboxOutline,
+    IoCheckmark,
+} from 'react-icons/io5';
+import {
     DateOutput,
+    TextOutput,
+    Button,
+    QuickActionButton,
+    Container,
 } from '@the-deep/deep-ui';
 
 import styles from './styles.css';
 
+interface Entry {
+    id: string;
+    sourceTitle?: string;
+    sourceCreatedOn?: string;
+    excerpt?: string;
+    sourcePublishedOn?: string;
+    sourceCreatedByDisplay?: string;
+    sourceAuthor?: string;
+    sourcePublisher?: string;
+}
+
 interface Props {
     className?: string;
-    leadTitle?: string;
-    leadCreatedOn?: string;
-    excerpt?: string;
+    entry: Entry
+    viewTags?: boolean;
+    onViewTagsButtonClick?: (entryId: string) => void;
+    onHideTagsButtonClick?: (entryId: string) => void;
 }
 
 function EntyrCard(props: Props) {
     const {
         className,
-        leadTitle,
-        leadCreatedOn,
-        excerpt,
+        entry,
+        viewTags,
+        onViewTagsButtonClick,
+        onHideTagsButtonClick,
     } = props;
 
     return (
-        <Card
-            className={_cs(styles.entryCard, className)}
-        >
-            <Heading>
-                {leadTitle}
-            </Heading>
-            {leadCreatedOn && (
-                <DateOutput
-                    value={leadCreatedOn}
-                />
+        <div
+            className={_cs(
+                styles.entryCard,
+                className,
+                viewTags && styles.expanded,
             )}
-            <div className={styles.excerpt}>
-                {excerpt}
-            </div>
-        </Card>
+        >
+            <Container
+                className={styles.sourceDetails}
+                heading={entry.sourceTitle}
+                headingSize="small"
+                headerDescription={entry.sourcePublishedOn && (
+                    <DateOutput
+                        value={entry.sourcePublishedOn}
+                    />
+                )}
+                footerQuickActions={(
+                    <>
+                        <QuickActionButton name={undefined}>
+                            <IoPencil />
+                        </QuickActionButton>
+                        <QuickActionButton name={undefined}>
+                            <IoTrash />
+                        </QuickActionButton>
+                    </>
+                )}
+                footerActions={(
+                    <Button
+                        name={entry.id}
+                        disabled={viewTags}
+                        onClick={onViewTagsButtonClick}
+                    >
+                        View tags
+                    </Button>
+                )}
+            >
+                <div>
+                    {entry.excerpt}
+                </div>
+                <div className={styles.metaSection}>
+                    <TextOutput
+                        label="Added on"
+                        value={entry.sourceCreatedOn}
+                        valueType="date"
+                    />
+                    <TextOutput
+                        label="Publisher"
+                        value={entry.sourcePublisher}
+                    />
+                    <TextOutput
+                        label="Added by"
+                        value={entry.sourceCreatedByDisplay}
+                    />
+                    <TextOutput
+                        label="Author"
+                        value={entry.sourceAuthor}
+                    />
+                </div>
+            </Container>
+            {viewTags && (
+                <>
+                    <div className={styles.verticalSeparator} />
+                    <Container
+                        className={styles.tagsContainer}
+                        headerActions={(
+                            <Button
+                                name={entry.id}
+                                onClick={onHideTagsButtonClick}
+                                variant="action"
+                            >
+                                <IoClose />
+                            </Button>
+                        )}
+                        footerActions={(
+                            <>
+                                <Button
+                                    name={entry.id}
+                                    variant="secondary"
+                                    icons={(
+                                        <IoPencil />
+                                    )}
+                                >
+                                    Edit Tags
+                                </Button>
+                                <QuickActionButton
+                                    className={styles.commentButton}
+                                    name={entry.id}
+                                >
+                                    <IoChatboxOutline />
+                                    <div className={styles.commentCount}>
+                                        {3}
+                                    </div>
+                                </QuickActionButton>
+                                <Button
+                                    name={entry.id}
+                                    variant="primary"
+                                    actionsClassName={styles.verifyActions}
+                                    actions={(
+                                        <>
+                                            <div>
+                                                {1}
+                                            </div>
+                                            <IoCheckmark />
+                                        </>
+                                    )}
+                                >
+                                    Verify
+                                </Button>
+                            </>
+                        )}
+                    >
+                        Tags
+                    </Container>
+                </>
+            )}
+        </div>
     );
 }
 
