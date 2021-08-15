44314f266684d29b0e07bc00d1cb707b38d08f9b
tnagorra <weathermist@gmail.com>
2021-08-15 20:14:08 +0545

    Fix minor issues
    - deepscan issues on hid config

--- app/Base/configs/hid.ts
+++ app/Base/configs/hid.ts
@@ -5,10 +5,9 @@ const state = '12345';
 // TODO: Add configs for local, dev and production
 // TODO: Add three types of env variable local, dev and production
 // instead of just development and production
-let clientId = process.env.REACT_APP_HID_CLIENT_ID;
-let redirectUrl = process.env.REACT_APP_HID_CLIENT_REDIRECT_URL;
-let hidAuthUrl = process.env.REACT_APP_HID_AUTH_URI;
-
+let clientId;
+let redirectUrl;
+let hidAuthUrl;
 if (process.env.REACT_APP_HID_CLIENT_ID) {
     clientId = process.env.REACT_APP_HID_CLIENT_ID;
     redirectUrl = process.env.REACT_APP_HID_CLIENT_REDIRECT_URL;
@@ -20,7 +19,5 @@ if (process.env.REACT_APP_HID_CLIENT_ID) {
 }
 
 // NOTE: HID doesn't seem to decode the params, so don't use `p` for now
-// eslint-disable-next-line max-len
-// export const hidUrl = `${hidAuthUrl}/oauth/authorize?${p({ response_type: responseType, client_id: clientId, scope, state, redirect_url: redirectUrl })}`;
 // eslint-disable-next-line import/prefer-default-export
 export const hidUrl = `${hidAuthUrl}/oauth/authorize?response_type=${responseType}&client_id=${clientId}&scope=${scope}&state=${state}&redirect_uri=${redirectUrl}`;
