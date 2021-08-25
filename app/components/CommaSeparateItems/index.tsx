35149caad20975d1a82c541581f6d11d675f536f
Sameer Rai <sameerai2736@gmail.com>
2021-08-25 18:16:35 +0545

    Add CommaSeparateItems components

---
+++ app/components/CommaSeparateItems/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { _cs } from '@togglecorp/fujs';
+
+import { joinList } from '#utils/common';
+
+import styles from './styles.css';
+
+interface Item {
+    id: number;
+    name: string;
+}
+interface Props {
+    className?: string;
+    items?: Item[];
+}
+
+function CommaSeparateItems(props: Props) {
+    const { items, className } = props;
+    if (!items || items.length < 1) {
+        return null;
+    }
+
+    const list = items.map((item: Item) => (
+        <span
+            key={item.id}
+            className={styles.item}
+        >
+            {item.name}
+        </span>
+    ));
+
+    return (
+        <span className={_cs(className, styles.items)}>
+            { joinList(list) }
+        </span>
+    );
+}
+
+export default CommaSeparateItems;
