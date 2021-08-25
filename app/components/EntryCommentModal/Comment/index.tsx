2a0d9a1b8815d98f7575401c9985135f0f63a9ec
Sameer Rai <sameerai2736@gmail.com>
2021-08-25 18:17:44 +0545

    List entry comments on modal

---
+++ app/components/EntryCommentModal/Comment/index.tsx
@@ -0,0 +1,138 @@
+import React, { useState, useCallback, useMemo, useContext } from 'react';
+import { _cs } from '@togglecorp/fujs';
+import { FiEdit2 } from 'react-icons/fi';
+import {
+    QuickActionButton,
+    DateOutput,
+    Container,
+    Card,
+} from '@the-deep/deep-ui';
+
+import UserContext from '#base/context/UserContext';
+import CommaSeparateItems from '#components/CommaSeparateItems';
+import _ts from '#ts';
+
+import { EntryComment } from '#types';
+//  import EditCommentForm from './EditCommentForm';
+import styles from './styles.css';
+
+interface Props {
+    className?: string;
+    comment: EntryComment;
+}
+
+const commentTypeToTextMap: { [id: number]: string } = {
+    0: 'commented',
+    1: 'verified',
+    2: 'unverified',
+    3: 'controlled',
+    4: 'uncontrolled',
+};
+
+function Comment(props: Props) {
+    const {
+        className,
+        comment: commentFromProps,
+    } = props;
+
+    const { user } = useContext(UserContext);
+    const [comment] = useState<EntryComment>(commentFromProps);
+    const [editMode, setEditMode] = useState<boolean>(false);
+
+    const {
+        textHistory,
+        createdByDetails,
+        commentType,
+        mentionedUsersDetails,
+        createdAt,
+    } = comment;
+    const [latest] = textHistory;
+
+    const isEditable = useMemo(() => (
+        user?.id === createdByDetails?.id.toString() && latest && !editMode
+    ), [user, createdByDetails, latest, editMode]);
+
+    const handleEditClick = useCallback(() => {
+        setEditMode(true);
+    }, []);
+
+    /* const handleCancel = useCallback(() => {
+     *     setEditMode(false);
+     * }, []);
+
+     * const handleSuccess = useCallback((value: EntryComment) => {
+     *     setComment(value);
+     *     setEditMode(false);
+     * }, []); */
+
+    return (
+        <Container
+            className={_cs(styles.commentContainer, className)}
+            headerClassName={styles.header}
+            headingSize="extraSmall"
+            headingContainerClassName={styles.headingContainer}
+            headingClassName={styles.heading}
+            heading={(
+                <>
+                    {createdByDetails?.name}
+                    &nbsp;
+                    <span className={styles.details}>
+                        {`${commentTypeToTextMap[commentType]} the entry on`}
+                    </span>
+                </>
+            )}
+            headerActions={(
+                <>
+                    <DateOutput
+                        value={createdAt}
+                    />
+                    {isEditable && (
+                        <QuickActionButton
+                            className={styles.button}
+                            name="editButton"
+                            onClick={handleEditClick}
+                            title="Edit comment"
+                        >
+                            <FiEdit2 />
+                        </QuickActionButton>
+                    )}
+                </>
+            )}
+            footerContentClassName={styles.footerContent}
+            footerContent={mentionedUsersDetails.length > 0 && (
+                <>
+                    Assigned to
+                    &nbsp;
+                    <CommaSeparateItems
+                        className={styles.users}
+                        items={mentionedUsersDetails}
+                    />
+                </>
+            )}
+            footerActions={textHistory.length > 1 && (
+                <span className={styles.modified}>
+                    &nbsp;
+                    Last modified on
+                    &nbsp;
+                    <DateOutput
+                        value={latest.createdAt}
+                    />
+                </span>
+            )}
+        >
+            {editMode ? (
+                <div className={styles.textContainer}>
+                    {latest?.text}
+                </div>
+            ) : (latest?.text && (
+                <Card
+                    className={styles.comment}
+                >
+                    {latest.text}
+                </Card>
+            ))}
+        </Container>
+    );
+}
+
+export default Comment;
