189fff3183e3bbe18b9d9f859c371104d9909101
Subina <subina@togglecorp.com>
2021-08-26 18:50:46 +0545

    Add filters in explore deep page

---
+++ app/views/ExploreDeep/ProjectFilterForm/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {
+    TextInput,
+} from '@the-deep/deep-ui';
+
+function ProjectFilterForm() {
+    return (
+        <div>
+            <TextInput
+                name="search"
+                value=""
+            />
+        </div>
+    );
+}
+
+export default ProjectFilterForm;
