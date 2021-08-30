b1e953b7fc2d8bcfa73580bd156de192999cef2d
Subina <subina@togglecorp.com>
2021-08-30 18:20:05 +0545

    Add Filter options in explore page

--- app/views/ExploreDeep/ProjectFilterForm/index.tsx
+++ app/views/ExploreDeep/ProjectFilterForm/index.tsx
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import {
     TextInput,
+    DateRangeInput,
+    Button,
 } from '@the-deep/deep-ui';
 
-function ProjectFilterForm() {
+// import OrganizationMultiSelectInput from '#components/OrganizationMultiSelectInput';
+
+interface Props {
+    filters: {
+        search: string;
+        organizations: string[];
+        createdAt: {
+            startDate: string;
+            endDate: string;
+        };
+    };
+}
+
+function ProjectFilterForm(props: Props) {
+    const {
+        filters,
+    } = props;
+
+    console.warn('filters', filters);
+
+    const [searchText, setSearchText] = useState<string>(search);
+
+    const handleSubmit = useCallback(() => {
+        console.warn('this is handle submit');
+    }, []);
+
     return (
         <div>
             <TextInput
                 name="search"
-                value=""
+                value={searchText}
+                onChange={setSearchText}
             />
+            <DateRangeInput
+                name="createdAt"
+                label="Added On"
+                value={{
+                    startDate,
+                    endDate,
+                }}
+            />
+            <Button
+                name="submit"
+                type="submit"
+                onClick={handleSubmit}
+            >
+                Submit
+            </Button>
         </div>
     );
 }
