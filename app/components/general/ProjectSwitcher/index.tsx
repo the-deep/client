5edf0b82e0b8e0cd490e2302ea5c9b6e7812039c
Aditya Khatri <adityakhatri47@gmail.com>
2021-09-06 22:21:07 +0545

    Add smart quick action link

--- app/components/general/ProjectSwitcher/index.tsx
+++ app/components/general/ProjectSwitcher/index.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import { FiEdit2 } from 'react-icons/fi';
+import { _cs } from '@togglecorp/fujs';
 import {
     generatePath,
     useRouteMatch,
@@ -7,11 +8,21 @@ import {
 } from 'react-router-dom';
 
 import ProjectSelectInput from '#components/selections/ProjectSelectInput';
-import SmartButtonLikeLink from '#base/components/SmartButtonLikeLink';
+import SmartQuickActionLink from '#base/components/SmartQuickActionLink';
 import ProjectContext from '#base/context/ProjectContext';
 import routes from '#base/configs/routes';
 
-function ProjectSwitcher() {
+import styles from './styles.css';
+
+interface Props {
+    className?: string;
+}
+
+function ProjectSwitcher(props: Props) {
+    const {
+        className,
+    } = props;
+
     const { project } = React.useContext(ProjectContext);
 
     const { path, params } = useRouteMatch();
@@ -26,7 +37,7 @@ function ProjectSwitcher() {
     );
 
     return (
-        <>
+        <div className={_cs(styles.projectSwitcher, className)}>
             <ProjectSelectInput
                 name="project"
                 value={project?.id}
@@ -35,15 +46,14 @@ function ProjectSwitcher() {
                 variant="general"
                 nonClearable
             />
-            <SmartButtonLikeLink
-                // FIXME: use SmartQuickButtonLikeLink
+            <SmartQuickActionLink
                 variant="secondary"
                 route={routes.projectEdit}
                 title="Edit Project"
             >
                 <FiEdit2 />
-            </SmartButtonLikeLink>
-        </>
+            </SmartQuickActionLink>
+        </div>
     );
 }
 export default ProjectSwitcher;
