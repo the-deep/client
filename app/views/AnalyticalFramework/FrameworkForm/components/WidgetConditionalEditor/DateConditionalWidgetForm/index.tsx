18571a9931b796744e7bc9a9788d92f408fd801e
tnagorra <weathermist@gmail.com>
2021-12-13 14:03:24 +0545

    Move parent widgets filtering logic inside WidgetConditionalEditor
    - Disable setting "parent widget" of a "parent widget"
    - Set "pristine" as false by default on WidgetConditionalEditor forms
    - Define widget filtering logic entry Review page component
        - Move logic inside Section and CompactSection

--- app/views/AnalyticalFramework/FrameworkForm/components/WidgetConditionalEditor/DateConditionalWidgetForm/index.tsx
+++ app/views/AnalyticalFramework/FrameworkForm/components/WidgetConditionalEditor/DateConditionalWidgetForm/index.tsx
@@ -323,7 +323,7 @@ function DateConditionalWidgetForm(props: DateConditionalWidgetFormProps) {
         setFieldValue,
         setValue,
         setError,
-    } = useForm(schema, initialValue);
+    } = useForm(schema, initialValue, false);
 
     const error = getErrorObject(riskyError);
 
--- app/views/AnalyticalFramework/FrameworkForm/components/WidgetConditionalEditor/TimeConditionalWidgetForm/index.tsx
+++ app/views/AnalyticalFramework/FrameworkForm/components/WidgetConditionalEditor/TimeConditionalWidgetForm/index.tsx
@@ -323,7 +323,7 @@ function TimeConditionalWidgetForm(props: TimeConditionalWidgetFormProps) {
         setFieldValue,
         setValue,
         setError,
-    } = useForm(schema, initialValue);
+    } = useForm(schema, initialValue, false);
 
     const error = getErrorObject(riskyError);
 
