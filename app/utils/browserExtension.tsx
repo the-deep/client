c124c51d25d980ca9c12273269f9dca23ae110c8
tnagorra <weathermist@gmail.com>
2022-01-19 16:24:36 +0545

    Use old extension id for screenshot

--- app/utils/browserExtension.tsx
+++ app/utils/browserExtension.tsx
@@ -1,4 +1,4 @@
-import { extensionId } from '#base/configs/env';
+import { oldExtensionId } from '#base/configs/env';
 
 // FIXME: Use new extension keys for the following after extension is deployed
 const EXTENSION_GET_SCREENSHOT = 'get-screenshot';
@@ -22,7 +22,7 @@ export const getScreenshot = () => {
             reject();
         }
 
-        chrome.runtime.sendMessage(extensionId, data, (response: ScreenshotResponse) => {
+        chrome.runtime.sendMessage(oldExtensionId, data, (response: ScreenshotResponse) => {
             if (!response) {
                 reject();
             }
@@ -45,7 +45,7 @@ export const sendToken = (token: unknown) => {
             reject();
         }
 
-        chrome.runtime.sendMessage(extensionId, data, (response) => {
+        chrome.runtime.sendMessage(oldExtensionId, data, (response) => {
             if (!response) {
                 reject();
             }
