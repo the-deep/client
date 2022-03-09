e799798f6e05d211840fde4416055998fa0edabc
tnagorra <weathermist@gmail.com>
2022-03-09 10:25:21 +0545

    Fix form schema for preditionTagMapping

--- app/views/AnalyticalFramework/schema.ts
+++ app/views/AnalyticalFramework/schema.ts
@@ -22,16 +22,40 @@ export type PartialFormType = PartialForm<FormType, 'primaryTagging' | 'secondar
 export type WidgetsType = NonNullable<PartialFormType['secondaryTagging']>;
 export type SectionsType = NonNullable<PartialFormType['primaryTagging']>;
 export type PropertiesType = NonNullable<PartialFormType['properties']>;
+export type PredictionTagMappingsType = NonNullable<PartialFormType['predictionTagsMapping']>;
 
 export type PartialWidgetsType = WidgetsType;
 export type PartialSectionsType = SectionsType;
+export type PartialPredictionTagMappingsType = PredictionTagMappingsType;
 
 // NOTE: These are not partials widget types even if it's on the name
 type PartialWidgetType = WidgetsType[number];
 type PartialSectionType = SectionsType[number];
+type PartialPredictionTagMappingType = PredictionTagMappingsType[number];
 
 type FormSchema = ObjectSchema<PartialFormType>;
 type FormSchemaFields = ReturnType<FormSchema['fields']>;
+
+type PredictionTagMappingSchema = ObjectSchema<PartialPredictionTagMappingType, PartialFormType>;
+type PredictionTagMappingSchemaFields = ReturnType<PredictionTagMappingSchema['fields']>;
+const predictionTagMappingSchema: PredictionTagMappingSchema = {
+    fields: (): PredictionTagMappingSchemaFields => ({
+        association: [],
+        tag: [],
+
+        widget: [],
+        widgetType: [],
+        clientId: [],
+        id: [defaultUndefinedType],
+    }),
+};
+
+type PredictionTagMappingsSchema = ArraySchema<PartialPredictionTagMappingType, PartialFormType>;
+type PredictionTagMappingsSchemaMember = ReturnType<PredictionTagMappingsSchema['member']>;
+const predictionTagMappingsSchema: PredictionTagMappingsSchema = {
+    keySelector: (col) => col.clientId,
+    member: (): PredictionTagMappingsSchemaMember => predictionTagMappingSchema,
+};
 
 type WidgetSchema = ObjectSchema<PartialWidgetType, PartialFormType>;
 type WidgetSchemaFields = ReturnType<WidgetSchema['fields']>;
@@ -102,7 +126,7 @@ const schema: FormSchema = {
             secondaryTagging: widgetsSchema,
 
             // FIXME: add stricter typings
-            predictionTagsMapping: [],
+            predictionTagsMapping: predictionTagMappingSchema,
         };
 
         if (value?.isVisualizationEnabled) {
