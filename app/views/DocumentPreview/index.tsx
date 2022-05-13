bff04929b80a591b6f6bf1b388645daf7b69d1ba
Subina <subina@togglecorp.com>
2022-05-13 18:59:54 +0545

    Add redirection to no permission users

--- app/views/DocumentPreview/index.tsx
+++ app/views/DocumentPreview/index.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useCallback, useMemo } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import { _cs, isDefined, doesObjectHaveNoData } from '@togglecorp/fujs';
+import { _cs } from '@togglecorp/fujs';
 import {
     IoDownloadOutline,
     IoCopyOutline,
@@ -11,27 +11,37 @@ import {
     Container,
     QuickActionButton,
     TextOutput,
+    DateOutput,
     useBooleanState,
     useAlert,
+    Button,
 } from '@the-deep/deep-ui';
 
 import LeadPreview from '#components/lead/LeadPreview';
+import routes from '#base/configs/routes';
+import SmartButtonLikeLink from '#base/components/SmartButtonLikeLink';
 import ProjectContext from '#base/context/ProjectContext';
 import UserContext from '#base/context/UserContext';
+import FullPageErrorMessage from '#views/FullPageErrorMessage';
+import { useModalState } from '#hooks/stateManagement';
+
+import ProjectJoinModal from '#views/ExploreDeep/ActionCell/ProjectJoinModal';
 
 import styles from './styles.css';
 
 interface Response {
     url: string;
+    projectId: string;
     lead: {
         id: string;
         title: string;
         authoringOrganization: string;
         publishedDate: string;
     };
+    canViewLead: boolean;
 }
 
-const fakeResponse: Response = {
+const mockResponse: Response = {
     url: 'https://www.onlinekhabar.com/2022/05/1123315',
     lead: {
         id: '1',
@@ -39,34 +49,37 @@ const fakeResponse: Response = {
         authoringOrganization: 'BBC',
         publishedDate: '2022-02-15T05:08:27.130741+00:00',
     },
+    canViewLead: false,
+    projectId: '1',
 };
 
 interface Props {
-    url?: string;
-    // attachment?: unknown;
     className?: string;
 }
 
 function DocumentPreview(props: Props) {
     const {
-        url = 'https://www.onlinekhabar.com/2022/05/1123315',
-        // TODO: remove this after connecting to API
-        // attachment,
         className,
     } = props;
     const { leadId } = useParams<{ leadId: string }>();
     const [infoPaneShown, showInfoPane, hideInfoPane] = useBooleanState(true);
     const { project } = useContext(ProjectContext);
-    const { user } = useContext(UserContext);
-    const isAuthenticated = !doesObjectHaveNoData(user);
-    const isProjectMember = isAuthenticated && isDefined(project?.currentUserRole);
+    const { authenticated } = useContext(UserContext);
+
+    const {
+        url,
+        lead,
+        canViewLead,
+        projectId,
+    } = mockResponse;
     const alert = useAlert();
     // TODO: Remove after redirection
-    console.warn('isProjectMember', isProjectMember);
-    // TODO: Remove after redirection
-    console.warn('isAuthenticated', isAuthenticated);
-    // TODO: Remove after redirection
     console.warn('lead ID', leadId);
+    const [
+        showProjectJoinModal,
+        setModalVisible,
+        setModalHidden,
+    ] = useModalState(false);
 
     const handleCopyToClipboard = useCallback(() => {
         navigator.clipboard.writeText(url ?? '');
@@ -77,16 +90,51 @@ function DocumentPreview(props: Props) {
                 variant: 'info',
             },
         );
-    }, [fakeResponse?.url, alert]);
+    }, [url, alert]);
 
     const handlePrintClick = useCallback(() => {
         window.print();
     }, []);
 
+    if (!authenticated && !canViewLead) {
+        return (
+            <FullPageErrorMessage
+                errorTitle="Not logged in"
+                errorMessage="You are not logged in. Try logging in."
+                krakenVariant="hi"
+                buttons={(
+                    <SmartButtonLikeLink
+                        variant="primary"
+                        route={routes.login}
+                    >
+                        Go to Login page
+                    </SmartButtonLikeLink>
+                )}
+            />
+        );
+    }
+
+    if (authenticated && !canViewLead) {
+        return (
+            <FullPageErrorMessage
+                errorTitle="Forbidden"
+                errorMessage="You cannot see this lead. Try joining project."
+                buttons={(
+                    <Button
+                        name={undefined}
+                        onClick={setModalVisible}
+                    >
+                        Request to join project
+                    </Button>
+                )}
+            />
+        );
+    }
+
     return (
         <Container
             className={_cs(className, styles.documentPreview)}
-            heading="Title of the document"
+            heading={lead?.title}
             borderBelowHeader
             contentClassName={styles.content}
             headerClassName={styles.header}
@@ -124,9 +172,8 @@ function DocumentPreview(props: Props) {
             >
                 <LeadPreview
                     className={styles.preview}
-                    url={url}
+                    url={mockResponse?.url}
                     hideBar
-                    // attachment={attachment}
                 />
             </div>
             {infoPaneShown && (
@@ -153,7 +200,7 @@ function DocumentPreview(props: Props) {
                     />
                     <TextOutput
                         label="Author"
-                        value={user?.displayName}
+                        value={lead?.authoringOrganization}
                         labelContainerClassName={styles.label}
                         valueContainerClassName={styles.value}
                     />
@@ -165,10 +212,22 @@ function DocumentPreview(props: Props) {
                     />
                     <TextOutput
                         label="Date of Publication"
-                        value="29 Mar, 2022"
+                        value={(<DateOutput value={lead?.publishedDate} />)}
                         labelContainerClassName={styles.label}
                         valueContainerClassName={styles.value}
                     />
+                    {showProjectJoinModal && (
+                        <ProjectJoinModal
+                            projectId={projectId}
+                            onModalClose={setModalHidden}
+                            onJoinRequestSuccess={() => {
+                                alert.show(
+                                    'Successfully sent project join request',
+                                    { variant: 'success' },
+                                );
+                            }}
+                        />
+                    )}
                 </Container>
             )}
         </Container>
