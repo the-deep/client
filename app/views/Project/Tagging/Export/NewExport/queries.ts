5e5ebf14eb437e1d74fe005754e1b5674abfa554
Sameer Rai <sameerai2736@gmail.com>
2022-05-20 14:07:36 +0545

    Add sources table and filters to new export page

---
+++ app/views/Project/Tagging/Export/NewExport/queries.ts
@@ -0,0 +1,80 @@
+import { gql } from '@apollo/client';
+
+export const PROJECT_FRAMEWORK_DETAILS = gql`
+    query ProjectFrameworkDetails($projectId: ID!) {
+        project(id: $projectId) {
+            id
+            analysisFramework {
+                id
+                exportables {
+                    data
+                    id
+                    inline
+                    order
+                    widgetKey
+                    widgetType
+                    widgetTypeDisplay
+                }
+                primaryTagging {
+                    widgets {
+                        id
+                        clientId
+                        key
+                        order
+                        properties
+                        conditional {
+                            parentWidget
+                            parentWidgetType
+                            conditions
+                        }
+                        title
+                        widgetId
+                        width
+                        version
+                    }
+                    clientId
+                    id
+                    order
+                    title
+                    tooltip
+                }
+                secondaryTagging {
+                    clientId
+                    id
+                    key
+                    order
+                    title
+                    properties
+                    conditional {
+                        parentWidget
+                        parentWidgetType
+                        conditions
+                    }
+                    widgetId
+                    width
+                    version
+                }
+            }
+        }
+    }
+`;
+
+export const CREATE_EXPORT = gql`
+    mutation CreateExport(
+        $projectId: ID!,
+        $data: ExportCreateInputType!,
+    ) {
+        project(id: $projectId) {
+            id
+            exportCreate(data: $data) {
+                ok
+                errors
+                result {
+                    id
+                    title
+                    isPreview
+                }
+            }
+        }
+    }
+`;
