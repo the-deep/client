3ecad7de32837d17db5c7058482d95afd89ca167
Aditya Khatri <adityakhatri47@gmail.com>
2023-01-08 14:34:13 +0545

    Add sparkline/step chart by project created dates

--- app/utils/common.ts
+++ app/utils/common.ts
@@ -259,3 +259,24 @@ export function mergeLists<T>(
 
     return finalList;
 }
+
+export function mergeItems<T, K extends string>(
+    list: T[],
+    keySelector: (item: T) => K,
+    merge: (prev: T, item: T, key: K) => T,
+): T[] {
+    const mapping: {
+        [key: string]: T | undefined;
+    } = {};
+    list.forEach((item) => {
+        const key = keySelector(item);
+        const prev = mapping[key];
+        if (!prev) {
+            mapping[key] = item;
+        } else {
+            mapping[key] = merge(prev, item, key);
+        }
+    });
+
+    return Object.values(mapping).filter(isDefined);
+}
