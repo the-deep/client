613ad0bb4ae3af1f214ad111049e0f51de657b53
Sameer Rai <sameerai2736@gmail.com>
2023-04-03 14:22:47 +0545

    feat: create EntryCard component for story analysis

---
+++ app/views/PillarAnalysis/AnalyticalStatementInput/StoryAnalysisModal/EntryCard/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { _cs } from '@togglecorp/fujs';
+import {
+    ContainerCard,
+    DateOutput,
+    ImagePreview,
+} from '@the-deep/deep-ui';
+import { IoPersonOutline } from 'react-icons/io5';
+
+import { EntryMin } from '#views/PillarAnalysis/context';
+import { organizationTitleSelector } from '#components/selections/NewOrganizationSelectInput';
+
+import styles from './styles.css';
+
+interface Props {
+    className?: string;
+    entry: EntryMin;
+}
+
+function EntryCard(props: Props) {
+    const {
+        className,
+        entry,
+    } = props;
+
+    const authors = entry.lead.authors
+        ?.map((author) => organizationTitleSelector(author)).join(',');
+
+    return (
+        <ContainerCard
+            className={_cs(className, styles.entryCard)}
+            headerClassName={styles.header}
+            headingSize="extraSmall"
+            headerActions={(
+                <DateOutput
+                    tooltip="Entry Created At"
+                    value={entry.createdAt}
+                />
+            )}
+            headerIcons={(
+                <IoPersonOutline title="authors" />
+            )}
+            heading={authors}
+        >
+            {entry.image?.file?.url && (
+                <ImagePreview
+                    alt={entry.image?.title}
+                    src={entry.image?.file?.url}
+                />
+            )}
+            {entry.excerpt}
+        </ContainerCard>
+    );
+}
+
+export default EntryCard;
