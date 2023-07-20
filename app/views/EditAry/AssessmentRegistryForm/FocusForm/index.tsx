fe7d51cd1690957496e4f2e746aa91b8814d008f
priyesh777 <priyeshgautam1010@gmail.com>
2023-07-20 16:46:51 +0545

    Add checkbox options for focus tab in assessment

--- app/views/EditAry/AssessmentRegistryForm/FocusForm/index.tsx
+++ app/views/EditAry/AssessmentRegistryForm/FocusForm/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { EntriesAsList, Error, SetBaseValueArg } from '@togglecorp/toggle-form';
+import {
+    EntriesAsList,
+    Error,
+    SetBaseValueArg,
+} from '@togglecorp/toggle-form';
+import { Checkbox } from '@the-deep/deep-ui';
 
 import { PartialFormType } from '../formSchema';
+import styles from './styles.css';
 
 interface Props {
     value: PartialFormType;
@@ -19,8 +25,76 @@ function FocusForm(props: Props) {
     } = props;
 
     return (
-        <div>
-            Focus Form
+        <div className={styles.cardCollection}>
+            <div className={styles.focusCard}>
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Context"
+                    name="confidentiality"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Displacement"
+                    name="bgCrisisType"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Casualities"
+                    name="affectedGroups"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+            </div>
+            <div className={styles.focusCard}>
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Context"
+                    name="confidentiality"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Displacement"
+                    name="bgCrisisType"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Casualities"
+                    name="affectedGroups"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+            </div>
+            <div className={styles.focusCard}>
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Context"
+                    name="confidentiality"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Displacement"
+                    name="bgCrisisType"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+                <Checkbox
+                    className={styles.focusOptions}
+                    label="Casualities"
+                    name="affectedGroups"
+                    value={undefined}
+                    onChange={setFieldValue}
+                />
+            </div>
         </div>
     );
 }
