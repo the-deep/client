fa73771f79e3f2609e653661c6e5751be1631ba9
puranban <puranban77@gmail.com>
2023-07-24 14:40:52 +0545

    Add focus checklist component

--- app/views/EditAry/AssessmentRegistryForm/FocusForm/index.tsx
+++ app/views/EditAry/AssessmentRegistryForm/FocusForm/index.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { EntriesAsList, Error, SetBaseValueArg } from '@togglecorp/toggle-form';
+import { gql, useQuery } from '@apollo/client';
+import { CheckListInput } from '@the-deep/deep-ui';
+
+import { enumKeySelector, enumLabelSelector } from '#utils/common';
+import { GetFocusOptionsQuery, GetFocusOptionsQueryVariables } from '#generated/types';
 
 import { PartialFormType } from '../formSchema';
+import styles from './styles.css';
 
+const GET_FOCUS_OPTIONS = gql`
+    query GetFocusOptions {
+        frameworkOptions: __type(name: "AssessmentRegistryFocusTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+        sectorOptions: __type(name: "AssessmentRegistrySectorTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+        protectionOptions: __type(name: "AssessmentRegistryProtectionInfoTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+        affectedGroupOptions: __type(name: "AssessmentRegistryAffectedGroupTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+    }
+`;
 interface Props {
     value: PartialFormType;
     setFieldValue: (...entries: EntriesAsList<PartialFormType>) => void;
@@ -18,9 +52,75 @@ function FocusForm(props: Props) {
         error,
     } = props;
 
+    const {
+        data,
+        loading,
+    } = useQuery<GetFocusOptionsQuery, GetFocusOptionsQueryVariables>(
+        GET_FOCUS_OPTIONS,
+    );
+
+    const [
+        frameworkOptions,
+        sectorOptions,
+        protectionOptions,
+        affectedOptions,
+    ] = useMemo(() => ([
+        data?.frameworkOptions?.enumValues ?? undefined,
+        data?.sectorOptions?.enumValues ?? undefined,
+        data?.protectionOptions?.enumValues ?? undefined,
+        data?.affectedGroupOptions?.enumValues ?? undefined,
+    ]), [data]);
+
     return (
-        <div>
-            Focus Form
+        <div className={styles.focus}>
+            <CheckListInput
+                listContainerClassName={styles.inputContainer}
+                labelContainerClassName={styles.label}
+                label="Framewok Pillars"
+                name="focuses"
+                direction="vertical"
+                keySelector={enumKeySelector}
+                labelSelector={enumLabelSelector}
+                value={value.focuses}
+                options={frameworkOptions}
+                onChange={setFieldValue}
+            />
+            <CheckListInput
+                listContainerClassName={styles.inputContainer}
+                labelContainerClassName={styles.label}
+                label="Humanitarian Sectors"
+                name="sectors"
+                direction="vertical"
+                keySelector={enumKeySelector}
+                labelSelector={enumLabelSelector}
+                value={value.sectors}
+                options={sectorOptions}
+                onChange={setFieldValue}
+            />
+            <CheckListInput
+                listContainerClassName={styles.inputContainer}
+                labelContainerClassName={styles.label}
+                label="Protection Information Management Systems"
+                name="protectionInfoMgmts"
+                direction="vertical"
+                keySelector={enumKeySelector}
+                labelSelector={enumLabelSelector}
+                value={value.protectionInfoMgmts}
+                options={protectionOptions}
+                onChange={setFieldValue}
+            />
+            <CheckListInput
+                listContainerClassName={styles.inputContainer}
+                labelContainerClassName={styles.label}
+                label="Affected Groups"
+                name="affectedGroups"
+                direction="vertical"
+                keySelector={enumKeySelector}
+                labelSelector={enumLabelSelector}
+                value={value.affectedGroups}
+                options={affectedOptions}
+                onChange={setFieldValue}
+            />
         </div>
     );
 }
