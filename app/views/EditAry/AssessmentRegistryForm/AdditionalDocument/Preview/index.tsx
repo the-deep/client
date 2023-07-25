f178360219bb3575821f9c9c2740608b40de06c5
puranban <puranban77@gmail.com>
2023-07-25 22:25:57 +0545

    Add link item view component

--- app/views/EditAry/AssessmentRegistryForm/AdditionalDocument/Preview/index.tsx
+++ app/views/EditAry/AssessmentRegistryForm/AdditionalDocument/Preview/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo } from 'react';
-import { isNotDefined } from '@togglecorp/fujs';
+import { isDefined, isNotDefined } from '@togglecorp/fujs';
 import { Modal } from '@the-deep/deep-ui';
 
 import LeadPreview from '#components/lead/LeadPreview';
@@ -10,12 +10,14 @@ import { FileUploadResponse } from '../types';
 interface Props {
     attachmentId?: string;
     onChangeSelectedAttachment: React.Dispatch<React.SetStateAction<string | undefined>>;
+    link?: string;
 }
 
 function Preview(props: Props) {
     const {
         attachmentId,
         onChangeSelectedAttachment,
+        link,
     } = props;
 
     const handleModalClose = useCallback(() => {
@@ -25,7 +27,7 @@ function Preview(props: Props) {
     const {
         response,
     } = useRequest<FileUploadResponse>({
-        skip: isNotDefined(attachmentId),
+        skip: isNotDefined(attachmentId) || isDefined(link),
         url: `server://files/${attachmentId}`,
     });
 
@@ -37,9 +39,21 @@ function Preview(props: Props) {
             file: response?.file ? { url: response?.file } : undefined,
         }), [response],
     );
+
+    const linkData = useMemo(
+        () => {
+            if (isNotDefined(link)) return null;
+            const linkValue = {
+                title: link,
+                file: link ? { url: link } : undefined,
+            };
+            return linkValue;
+        }, [link],
+    );
+
     return (
         <Modal onCloseButtonClick={handleModalClose}>
-            <LeadPreview attachment={attachmentData} />
+            <LeadPreview attachment={linkData ?? attachmentData} />
         </Modal>
     );
 }
