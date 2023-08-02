8d4e1c7783873dcea86cf7561d1e811d7928d2a7
shreeyash07 <shresthashree0807@gmail.com>
2023-08-02 18:43:42 +0545

    Add input components

--- app/views/EditAry/AssessmentRegistryForm/ScoreForm/QualityScoreForm/index.tsx
+++ app/views/EditAry/AssessmentRegistryForm/ScoreForm/QualityScoreForm/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Heading } from '@the-deep/deep-ui';
 
+import QualityScoreInput from './QualityScoreInput';
 import styles from './styles.css';
 
 function QualityScoreForm() {
     return (
-        <div className={styles.qualityScoreForm}>quality score</div>
+        <div className={styles.qualityScoreForm}>
+            <div className={styles.scoreContent}>
+                <Heading
+                    size="extraSmall"
+                    className={styles.scoreHeading}
+                >
+                    Fit For Purpose
+                </Heading>
+                <Heading
+                    size="extraSmall"
+                    className={styles.scoreHeading}
+                >
+                    Score
+                </Heading>
+                <Heading
+                    size="extraSmall"
+                    className={styles.scoreHeading}
+                >
+                    Justification
+                </Heading>
+            </div>
+            <QualityScoreInput />
+        </div>
     );
 }
 
