8978d1b28cb298107e0a3a73cf5079bfdc1f98a6
puranban <puranban77@gmail.com>
2023-09-08 00:29:14 +0545

    Add map visualization for how assessed in ary dashboard

--- app/views/AryDashboard/HowAssessed/index.tsx
+++ app/views/AryDashboard/HowAssessed/index.tsx
@@ -1,18 +1,142 @@
 import React from 'react';
-import { _cs } from '@togglecorp/fujs';
+import { _cs, isNotDefined } from '@togglecorp/fujs';
+import { PurgeNull, removeNull } from '@togglecorp/toggle-form';
+import { gql, useQuery } from '@apollo/client';
+
+import {
+    AryDashboardFilterQuery,
+    AryDashboardFilterQueryVariables,
+    AryDashboardHowAssedQuery,
+    AryDashboardHowAssedQueryVariables,
+    GetMethodologyOptionsQuery,
+    GetMethodologyOptionsQueryVariables,
+} from '#generated/types';
+
+import GeographicalAreaMethodology from './GeographicalAreaMethodology';
+
+const GET_METHODOLOGY_OPTIONS = gql`
+    query GetMethodologyOptions {
+        dataCollectionTechniqueOptions: __type(name: "AssessmentRegistryDataCollectionTechniqueTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+        samplingApproach: __type(name: "AssessmentRegistrySamplingApproachTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+        proximity: __type(name: "AssessmentRegistryProximityTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+        unitOfAnanlysis: __type(name: "AssessmentRegistryUnitOfAnalysisTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+        unitOfReporting: __type(name: "AssessmentRegistryUnitOfReportingTypeEnum") {
+            enumValues {
+                name
+                description
+            }
+        }
+    }
+`;
+const ARY_DASHBOARD_HOW_ASSESSED = gql`
+    query AryDashboardHowAssed(
+        $projectId: ID!,
+        $filter: AssessmentDashboardFilterInputType!,
+    ) {
+        project(id: $projectId) {
+            id
+            assessmentDashboardStatistics(filter: $filter){
+                assessmentByDataCollectionTechniqueAndGeolocation {
+                    adminLevelId
+                    count
+                    geoId
+                    region
+                    dataCollectionTechnique
+                }
+                assessmentBySamplingApproachAndGeolocation {
+                    adminLevelId
+                    count
+                    geoId
+                    region
+                    samplingApproach
+                }
+                assessmentByUnitOfAnalysisAndGeolocation {
+                    adminLevelId
+                    count
+                    geoId
+                    region
+                    unitOfAnalysis
+                }
+                assessmentByUnitOfReportingAndGeolocation {
+                    adminLevelId
+                    count
+                    geoId
+                    region
+                    unitOfReporting
+                }
+                assessmentByProximityAndGeolocation {
+                    adminLevelId
+                    count
+                    geoId
+                    region
+                    proximity
+                }
+            }
+        }
+    }
+`;
 
 interface Props {
     className?: string;
+    filterVariables?: AryDashboardFilterQueryVariables;
+    regions: NonNullable<PurgeNull<AryDashboardFilterQuery['project']>>['regions'];
 }
 
 function HowAssessed(props: Props) {
     const {
         className,
+        filterVariables,
+        regions,
     } = props;
+
+    const {
+        loading,
+        data: options,
+    } = useQuery<GetMethodologyOptionsQuery, GetMethodologyOptionsQueryVariables>(
+        GET_METHODOLOGY_OPTIONS,
+    );
+
+    const {
+        loading: responsePending,
+        data,
+    } = useQuery<AryDashboardHowAssedQuery, AryDashboardHowAssedQueryVariables>(
+        ARY_DASHBOARD_HOW_ASSESSED,
+        {
+            skip: isNotDefined(filterVariables),
+            variables: filterVariables,
+        },
+    );
+
+    const filterData = removeNull(data?.project?.assessmentDashboardStatistics);
 
     return (
         <div className={_cs(className)}>
-            How was it assessed?
+            <GeographicalAreaMethodology
+                data={filterData}
+                options={options}
+                regions={regions}
+                navigationDisabled={loading || responsePending}
+            />
         </div>
     );
 }
