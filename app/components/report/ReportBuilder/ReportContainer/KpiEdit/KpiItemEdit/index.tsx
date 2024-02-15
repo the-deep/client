0569ba561ce8e53867a9857f93e98f6ad1f859b5
Subina <subina@togglecorp.com>
2024-02-15 16:20:07 +0545

    Add Kpi edits and display

--- app/components/report/ReportBuilder/ReportContainer/KpiEdit/KpiItemEdit/index.tsx
+++ app/components/report/ReportBuilder/ReportContainer/KpiEdit/KpiItemEdit/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { randomString } from '@togglecorp/fujs';
 import {
-    Container,
-    TextInput,
+    ExpandableContainer,
+    DateInput,
     NumberInput,
     QuickActionButton,
+    Switch,
+    TextInput,
 } from '@the-deep/deep-ui';
 import {
     SetValueArg,
@@ -17,6 +19,8 @@ import { IoTrash } from 'react-icons/io5';
 import {
     type FinalKpiItemType,
 } from '../../../../schema';
+
+import styles from './styles.css';
 
 type KpiItemType = FinalKpiItemType;
 
@@ -57,8 +61,8 @@ function KpiItemEdit(props: Props) {
     );
 
     return (
-        <Container
-            heading={`Item ${index + 1}`}
+        <ExpandableContainer
+            heading={value.title ?? `Item: ${index + 1}`}
             headerActions={(
                 <QuickActionButton
                     title="Remove Attributes"
@@ -68,13 +72,24 @@ function KpiItemEdit(props: Props) {
                     <IoTrash />
                 </QuickActionButton>
             )}
+            contentClassName={styles.kpiItem}
+            withoutBorder
         >
             <TextInput
                 label="Title"
                 name="title"
                 onChange={onFieldChange}
-                // error={error?.title}
+                error={error?.title}
                 value={value.title}
+                disabled={disabled}
+                readOnly={readOnly}
+            />
+            <TextInput
+                label="Subtitle"
+                name="subtitle"
+                onChange={onFieldChange}
+                error={error?.subtitle}
+                value={value.subtitle}
                 disabled={disabled}
                 readOnly={readOnly}
             />
@@ -82,12 +97,48 @@ function KpiItemEdit(props: Props) {
                 label="value"
                 name="value"
                 onChange={onFieldChange}
-                // error={error?.value}
+                error={error?.value}
                 value={value.value}
                 disabled={disabled}
                 readOnly={readOnly}
             />
-        </Container>
+            <Switch
+                label="Abbreviated value"
+                name="abbreviateValue"
+                onChange={onFieldChange}
+                // error={error?.abbreviateValue}
+                value={value.abbreviateValue}
+                disabled={disabled}
+                readOnly={readOnly}
+            />
+            <TextInput
+                label="Source title"
+                name="source"
+                onChange={onFieldChange}
+                error={error?.source}
+                value={value.source}
+                disabled={disabled}
+                readOnly={readOnly}
+            />
+            <TextInput
+                label="Source URL"
+                name="sourceUrl"
+                onChange={onFieldChange}
+                error={error?.sourceUrl}
+                value={value.sourceUrl}
+                disabled={disabled}
+                readOnly={readOnly}
+            />
+            <DateInput
+                label="Date"
+                name="date"
+                onChange={onFieldChange}
+                error={error?.date}
+                value={value.date}
+                disabled={disabled}
+                readOnly={readOnly}
+            />
+        </ExpandableContainer>
     );
 }
 
